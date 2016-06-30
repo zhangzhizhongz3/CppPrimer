85112769123b314beb5b362d32477449d32bd1ed
zzz <zhangzhizhongz3@foxmail.com>
2016-06-30 20:11:45 +0800

    Update ex5_21.cpp

--- ch05/ex5_21.cpp
+++ ch05/ex5_21.cpp
@@ -22,19 +22,19 @@ using std::string;
 int main()
 {
     string curr, prev;
-    bool no_twice = false;
+    bool no_twice = true;
 
     while (cin >> curr) {
         if (!isupper(curr[0]))
             continue;
         else if (prev == curr) {
             cout << curr << " occurs twice in succession." << endl;
-            no_twice = true;
+            no_twice = false;
             break;
         }
         else
             prev = curr;
     }
 
-    if (!no_twice) cout << "no word was repeated." << endl;
+    if (no_twice) cout << "no word was repeated." << endl;
 }
