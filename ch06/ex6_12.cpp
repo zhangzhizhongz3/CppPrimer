e12cc1ae81bc37952d5f0e00b68f0bc8a9004290
zzz <zhangzhizhongz3@foxmail.com>
2016-07-03 20:54:09 +0800

    Update ex6_12.cpp

--- ch06/ex6_12.cpp
+++ ch06/ex6_12.cpp
@@ -4,7 +4,7 @@
 //! Rewrite the program from exercise 6.10 in § 6.2.1 (p. 210) to use
 //! references instead of pointers to swap the values of two ints. Which
 //! version do you think would be easier to use and why?
-//  The version using reference is easier.
+//  The version using references is easier.
 //!
 #include <iostream>
 
