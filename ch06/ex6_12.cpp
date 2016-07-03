ef34506e0bcfa4cf93d36be397c34b0888469664
zzz <zhangzhizhongz3@foxmail.com>
2016-07-03 20:51:58 +0800

    Update ex6_12.cpp

--- ch06/ex6_12.cpp
+++ ch06/ex6_12.cpp
@@ -2,12 +2,11 @@
 //!
 //! Exercise 6.12:
 //! Rewrite the program from exercise 6.10 in § 6.2.1 (p. 210) to use
-//! references instead of pointers to swap the value of two ints. Which
+//! references instead of pointers to swap the values of two ints. Which
 //! version do you think would be easier to use and why?
 //  The version using reference is easier.
 //!
 #include <iostream>
-#include <string>
 
 void swap(int& lhs, int& rhs)
 {
