79c3fd7e72a55d6f671e9065776e552f5cd7829d
zzz <zhangzhizhongz3@foxmail.com>
2016-07-06 21:27:57 +0800

    Update ex6_42.cpp

--- ch06/ex6_42.cpp
+++ ch06/ex6_42.cpp
@@ -11,22 +11,21 @@
 
 #include <iostream>
 #include <string>
+#include <cstddef>
 
 using std::string;
 using std::cout;
 using std::endl;
 
-string make_plural(size_t ctr, const string& word, const string& ending = "s")
+string make_plural(size_t ctr, const string &word, const string &ending = "s")
 {
     return (ctr > 1) ? word + ending : word;
 }
 
 int main()
 {
-    cout << "singual: " << make_plural(1, "success", "es") << " "
+    cout << "singular: " << make_plural(1, "success", "es") << " "
          << make_plural(1, "failure") << endl;
     cout << "plural : " << make_plural(2, "success", "es") << " "
          << make_plural(2, "failure") << endl;
-
-    return 0;
 }
