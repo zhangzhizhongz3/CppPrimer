d874509908c9f239def67027ae9a3c907a3c703e
zzz <zhangzhizhongz3@foxmail.com>
2016-07-11 21:13:45 +0800

    Update ex7_02.h

--- ch07/ex7_02.h
+++ ch07/ex7_02.h
@@ -12,7 +12,7 @@
 #include <string>
 
 struct Sales_data {
-    std::string isbn() const { return bookNo; };
+    std::string isbn() const { return bookNo; }
     Sales_data& combine(const Sales_data&);
 
     std::string bookNo;
