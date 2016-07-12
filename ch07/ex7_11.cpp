fa73f053f9759c10aeab89afa260e87f496594b1
zzz <zhangzhizhongz3@foxmail.com>
2016-07-12 23:02:25 +0800

    Update ex7_11.cpp

--- ch07/ex7_11.cpp
+++ ch07/ex7_11.cpp
@@ -23,7 +23,7 @@ int main()
     Sales_data item3("0-201-78345-X", 3, 20.00);
     print(cout, item3) << endl;
 
-    Sales_data item4(std::cin);
+    Sales_data item4(cin);
     print(cout, item4) << endl;
 
 }
