10886f920c93d5e46d67761616e143055b19622e
zzz <zhangzhizhongz3@foxmail.com>
2016-07-12 22:58:03 +0800

    Update ex7_11.cpp

--- ch07/ex7_11.cpp
+++ ch07/ex7_11.cpp
@@ -7,20 +7,23 @@
 //
 
 #include "ex7_11.h"
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
 
 int main()
 {
     Sales_data item1;
-    print(std::cout, item1) << std::endl;
+    print(cout, item1) << endl;
 
     Sales_data item2("0-201-78345-X");
-    print(std::cout, item2) << std::endl;
+    print(cout, item2) << endl;
 
     Sales_data item3("0-201-78345-X", 3, 20.00);
-    print(std::cout, item3) << std::endl;
+    print(cout, item3) << endl;
 
     Sales_data item4(std::cin);
-    print(std::cout, item4) << std::endl;
+    print(cout, item4) << endl;
 
-    return 0;
 }
