fa94478276ba0b46d5e44ca4e22063e72070acda
zzz <zhangzhizhongz3@foxmail.com>
2016-07-12 11:53:41 +0800

    Update ex7_09.h

--- ch07/ex7_09.h
+++ ch07/ex7_09.h
@@ -12,18 +12,18 @@
 #include <string>
 #include <iostream>
 
-struct Person {
-    const std::string& getName() const { return name; }
-    const std::string& getAddress() const { return address; }
-
+class Person {
     std::string name;
     std::string address;
+    
+public:
+    const std::string& getName() const { return name; }
+    const std::string& getAddress() const { return address; }
 };
 
 std::istream& read(std::istream& is, Person& person)
 {
     is >> person.name >> person.address;
-    if (!is) person = Person();
     return is;
 }
 
