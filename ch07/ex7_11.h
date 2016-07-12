dbe9bbca6229e7e3e9401272bd96e4adf96a1053
zzz <zhangzhizhongz3@foxmail.com>
2016-07-12 22:11:39 +0800

    Update ex7_11.h

--- ch07/ex7_11.h
+++ ch07/ex7_11.h
@@ -15,19 +15,29 @@
 struct Sales_data {
     Sales_data() = default;
     Sales_data(const std::string& s) : bookNo(s) {}
-    Sales_data(const std::string& s, unsigned n, double p)
-        : bookNo(s), units_sold(n), revenue(n * p)
-    {
-    }
-    Sales_data(std::istream& is);
+    Sales_data(const std::string& s, unsigned n, double p) : bookNo(s), units_sold(n), revenue(p * n) {}
+    Sales_data(std::istream&);
 
-    std::string isbn() const { return bookNo; };
+    std::string isbn() const { return bookNo; }
     Sales_data& combine(const Sales_data&);
 
     std::string bookNo;
     unsigned units_sold = 0;
     double revenue = 0.0;
 };
+
+// member functions
+Sales_data::Sales_data(std::istream& is)
+{
+    read(is, *this);
+}
+
+Sales_data& Sales_data::combine(const Sales_data& rhs)
+{
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
+    return *this;
+}
 
 // nonmember functions
 std::istream& read(std::istream& is, Sales_data& item)
@@ -49,19 +59,6 @@ Sales_data add(const Sales_data& lhs, const Sales_data& rhs)
     Sales_data sum = lhs;
     sum.combine(rhs);
     return sum;
-}
-
-// member functions.
-Sales_data::Sales_data(std::istream& is)
-{
-    read(is, *this);
-}
-
-Sales_data& Sales_data::combine(const Sales_data& rhs)
-{
-    units_sold += rhs.units_sold;
-    revenue += rhs.revenue;
-    return *this;
 }
 
 #endif
