3d8666fdba3b1330cfe6ea7f7f3e3ae51966e24e
zzz <zhangzhizhongz3@foxmail.com>
2016-07-12 10:16:14 +0800

    Update ex7_06.h

--- ch07/ex7_06.h
+++ ch07/ex7_06.h
@@ -21,7 +21,7 @@ struct Sales_data {
     double revenue = 0.0;
 };
 
-// member functions.
+// member functions
 Sales_data& Sales_data::combine(const Sales_data& rhs)
 {
     units_sold += rhs.units_sold;
