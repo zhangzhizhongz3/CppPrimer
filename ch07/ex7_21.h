8325b9628f708a90bb96aa107be90cff8bcd3b83
zzz <zhangzhizhongz3@foxmail.com>
2016-07-13 21:01:44 +0800

    Update ex7_21.h

--- ch07/ex7_21.h
+++ ch07/ex7_21.h
@@ -13,20 +13,17 @@
 #include <iostream>
 
 class Sales_data {
-    friend std::istream& read(std::istream& is, Sales_data& item);
-    friend std::ostream& print(std::ostream& os, const Sales_data& item);
-    friend Sales_data add(const Sales_data& lhs, const Sales_data& rhs);
+friend std::istream& read(std::istream&, Sales_data&);
+friend std::ostream& print(std::ostream&, const Sales_data&);
+friend Sales_data add(const Sales_data&, const Sales_data&);
 
 public:
     Sales_data() = default;
     Sales_data(const std::string& s) : bookNo(s) {}
-    Sales_data(const std::string& s, unsigned n, double p)
-        : bookNo(s), units_sold(n), revenue(n * p)
-    {
-    }
+    Sales_data(const std::string& s, unsigned n, double p) : bookNo(s), units_sold(n), revenue(p * n) {}
     Sales_data(std::istream& is) { read(is, *this); }
 
-    std::string isbn() const { return bookNo; };
+    std::string isbn() const { return bookNo; }
     Sales_data& combine(const Sales_data&);
 
 private:
@@ -35,7 +32,7 @@ private:
     double revenue = 0.0;
 };
 
-// member functions.
+// member functions
 Sales_data& Sales_data::combine(const Sales_data& rhs)
 {
     units_sold += rhs.units_sold;
