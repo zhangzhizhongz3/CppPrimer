664357122ee2c26f8b53df7de1487b701aa77975
zzz <zhangzhizhongz3@foxmail.com>
2016-07-14 23:03:20 +0800

    Update ex7_26.h

--- ch07/ex7_26.h
+++ ch07/ex7_26.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 pezy. All rights reserved.
 //
 //  @See ex7_21.h
-//  @Add inline member function "Sales_data::avg_pric"
+//  @Add inline member function "Sales_data::avg_price"
 
 #ifndef CP5_ex7_26_h
 #define CP5_ex7_26_h
@@ -22,10 +22,7 @@ class Sales_data {
 public:
     Sales_data() = default;
     Sales_data(const std::string& s) : bookNo(s) {}
-    Sales_data(const std::string& s, unsigned n, double p)
-        : bookNo(s), units_sold(n), revenue(n * p)
-    {
-    }
+    Sales_data(const std::string& s, unsigned n, double p) : bookNo(s), units_sold(n), revenue(n * p) {}
     Sales_data(std::istream& is) { read(is, *this); }
 
     std::string isbn() const { return bookNo; };
@@ -40,7 +37,7 @@ private:
     double revenue = 0.0;
 };
 
-inline double Sales_data::avg_price() const
+double Sales_data::avg_price() const
 {
     return units_sold ? revenue / units_sold : 0;
 }
