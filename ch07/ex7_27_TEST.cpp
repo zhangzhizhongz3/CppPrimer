4d337fdf556e07276daf9cc714e3b2d7ba7dee3c
zzz <zhangzhizhongz3@foxmail.com>
2016-07-15 17:26:58 +0800

    Update ex7_27_TEST.cpp

--- ch07/ex7_27_TEST.cpp
+++ ch07/ex7_27_TEST.cpp
@@ -7,14 +7,15 @@
 //
 
 #include "ex7_27.h"
+#include <iostream>
+using std::cout;
 
 int main()
 {
     Screen myScreen(5, 5, 'X');
-    myScreen.move(4, 0).set('#').display(std::cout);
-    std::cout << "\n";
-    myScreen.display(std::cout);
-    std::cout << "\n";
+    myScreen.move(4, 0).set('#').display(cout);
+    cout << "\n";
+    myScreen.display(cout);
+    cout << "\n";
 
-    return 0;
 }
