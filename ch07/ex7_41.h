9da7fe4e7125083dbad5dcc19ce88050a642b3d5
zzz <zhangzhizhongz3@foxmail.com>
2016-07-17 23:03:54 +0800

    Update ex7_41.h

--- ch07/ex7_41.h
+++ ch07/ex7_41.h
@@ -16,49 +16,53 @@
 #include <string>
 #include <iostream>
 
+class Sales_data;
+std::istream &read(std::istream&, Sales_data&);
+
 class Sales_data {
-    friend std::istream& read(std::istream& is, Sales_data& item);
-    friend std::ostream& print(std::ostream& os, const Sales_data& item);
-    friend Sales_data add(const Sales_data& lhs, const Sales_data& rhs);
+    friend std::istream& read(std::istream&, Sales_data&);
+    friend std::ostream& print(std::ostream&, const Sales_data&);
+    friend Sales_data add(const Sales_data&, const Sales_data&);
 
 public:
-    Sales_data(const std::string& s, unsigned n, double p)
-        : bookNo(s), units_sold(n), revenue(n * p)
+    Sales_data(const std::string& s, unsigned n, double p) : bookNo(s), units_sold(n), revenue(p * n)
     {
-        std::cout << "Sales_data(const std::string&, unsigned, double)"
-                  << std::endl;
+        std::cout << "Sales_data(const std::string&, unsigned, double)" << std::endl;
     }
 
-    Sales_data() : Sales_data("", 0, 0.0f)
+    Sales_data() : Sales_data("", 0, 0)
     {
         std::cout << "Sales_data()" << std::endl;
     }
 
-    Sales_data(const std::string& s) : Sales_data(s, 0, 0.0f)
+    Sales_data(const std::string& s) : Sales_data(s, 0, 0)
     {
         std::cout << "Sales_data(const std::string&)" << std::endl;
     }
 
-    Sales_data(std::istream& is);
-
+    Sales_data(std::istream& is) : Sales_data()
+    {
+        read(is, *this);
+        std::cout<<"Sales_data(std::istream&)"<<std::endl;
+    }
+    
     std::string isbn() const { return bookNo; }
     Sales_data& combine(const Sales_data&);
 
 private:
     inline double avg_price() const;
 
-private:
     std::string bookNo;
     unsigned units_sold = 0;
     double revenue = 0.0;
 };
 
-inline double Sales_data::avg_price() const
+double Sales_data::avg_price() const
 {
     return units_sold ? revenue / units_sold : 0;
 }
 
-// declarations for nonmember parts of the Sales_data interface.
+// declarations for nonmember parts of the Sales_data interface
 std::istream& read(std::istream& is, Sales_data& item);
 std::ostream& print(std::ostream& os, const Sales_data& item);
 Sales_data add(const Sales_data& lhs, const Sales_data& rhs);
