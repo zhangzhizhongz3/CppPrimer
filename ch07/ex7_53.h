a73c6c97a637c51a5c381b1a363ca3a13f85e5ee
zzz <zhangzhizhongz3@foxmail.com>
2016-07-19 09:03:08 +0800

    Update ex7_53.h

--- ch07/ex7_53.h
+++ ch07/ex7_53.h
@@ -12,7 +12,7 @@
 class Debug {
 public:
     constexpr Debug(bool b = true) : rt(b), io(b), other(b) {}
-    constexpr Debug(bool r, bool i, bool o) : rt(r), io(i), other(0) {}
+    constexpr Debug(bool r, bool i, bool o) : rt(r), io(i), other(o) {}
     constexpr bool any() { return rt || io || other; }
 
     void set_rt(bool b) { rt = b; }
@@ -21,7 +21,7 @@ public:
 
 private:
     bool rt;    // runtime error
-    bool io;    // I/O error
+    bool io;    // IO error
     bool other; // the others
 };
 
