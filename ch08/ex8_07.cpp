7988c2f33882975b7936a6084561ae689dc53387
zzz <zhangzhizhongz3@foxmail.com>
2016-07-22 21:32:02 +0800

    Update ex8_07.cpp

--- ch08/ex8_07.cpp
+++ ch08/ex8_07.cpp
@@ -5,42 +5,41 @@
 //  Created by pezy on 11/28/14.
 //  Copyright (c) 2014 pezy. All rights reserved.
 //
-//  @Brief  Revise the bookstore program from the previous section to write its
-//  output to a file.
+//  @Brief  Revise the bookstore program from the previous section to write its output to a file.
 //          Pass the name of that file as a second argument to main.
 //  @See    ex8_06.cpp
-//  @Run    give a parameter: "../data/book.txt ../data/out.txt"
+//  @Run    give arguments: "E:\book_sales.txt" "E:\zzz.txt"
 
-#include <fstream>
 #include <iostream>
-
-#include "../ch07/ex7_26.h"
+using std::cerr; using std::cin; using std::cout; using std::endl;
+#include <fstream>
 using std::ifstream;
-using std::ofstream;
-using std::endl;
-using std::cerr;
+using std::ofstream;    
 
-int main(int argc, char** argv)
+#include "ex7_26.h"
+
+int main(int argc, char *argv[])
 {
-    ifstream input(argv[1]);
-    ofstream output(argv[2]);
-
-    Sales_data total;
-    if (read(input, total)) {
-        Sales_data trans;
-        while (read(input, trans)) {
-            if (total.isbn() == trans.isbn())
-                total.combine(trans);
-            else {
-                print(output, total) << endl;
-                total = trans;
-            }
-        }
-        print(output, total) << endl;
-    }
-    else {
-        cerr << "No data?!" << endl;
-    }
-
-    return 0;
+	ifstream input(argv[1]);
+	ofstream output(argv[2]);
+	Sales_data total;
+	if (read(input, total))
+	{
+		Sales_data trans;
+		while(read(input, trans))
+		{
+			if (total.isbn() == trans.isbn())
+				total.combine(trans);
+			else
+			{
+				print(output, total) << endl;
+				total = trans;
+			}
+		}
+		print(output, total) << endl;
+	}
+	else
+	{
+		cerr << "No data?!" << endl;
+	}
 }
