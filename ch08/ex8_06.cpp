e082bfc926bd4a67b47e392c4d292a5a297ee25e
zzz <zhangzhizhongz3@foxmail.com>
2016-07-22 17:35:51 +0800

    Update ex8_06.cpp

--- ch08/ex8_06.cpp
+++ ch08/ex8_06.cpp
@@ -5,43 +5,53 @@
 //  Created by pezy on 11/27/14.
 //  Copyright (c) 2014 pezy. All rights reserved.
 //
-//  @Brief  Rewrite the bookstore program from ¡ì7.1.1 (p.256) to read its
-//  transactions from a file.
-//          Pass the name of the file as an argument to main (¡ì6.2.5, p.218).
+//  @Brief  Rewrite the bookstore program from 7.1.1 (p.256) to read its transactions from a file.
+//          Pass the name of the file as an argument to main (6.2.5, p.218).
 //  @See    ex7_26.h (use the Sales_data)
-//  @Run    give a parameter: "../data/book.txt"
-//  @Output 0-201-78345-X 5 110
-//          0-201-78346-X 9 839.2
+//  @Run    give a parameter: "E:\book_sales.txt"
+//  @Output 0-201-70353-X 4 99.96 24.99
+//          0-201-82470-1 4 181.56 45.39
+//          0-201-88954-4 16 198 12.375
+//          0-399-82477-1 5 226.95 45.39
+//          0-201-78345-X 5 110 22
 
-#include <fstream>
+// @Brief   Rewrite the bookstore program from 7.1.1 (p.256) to read its transactions from a file.
+//          Pass the name of the file as an argument to main (6.2.5, p.218).
+// @See     ex7_26.h (use the Sales_data)
+// @Run     give an argument: "E:\book_sales.txt"
+// @Output  0-201-70353-X 4 99.96 24.99
+//          0-201-82470-1 4 181.56 45.39
+//          0-201-88954-4 16 198 12.375
+//          0-399-82477-1 5 226.95 45.39
+//          0-201-78345-X 5 110 22
 #include <iostream>
-
-#include "../ch07/ex7_26.h"
+using std::cerr; using std::cin; using std::cout; using std::endl;
+#include <fstream>
 using std::ifstream;
-using std::cout;
-using std::endl;
-using std::cerr;
 
-int main(int argc, char** argv)
+#include "ex7_26.h"
+
+int main(int argc, char *argv[])
 {
-    ifstream input(argv[1]);
-
-    Sales_data total;
-    if (read(input, total)) {
-        Sales_data trans;
-        while (read(input, trans)) {
-            if (total.isbn() == trans.isbn())
-                total.combine(trans);
-            else {
-                print(cout, total) << endl;
-                total = trans;
-            }
-        }
-        print(cout, total) << endl;
-    }
-    else {
-        cerr << "No data?!" << endl;
-    }
-
-    return 0;
+	ifstream input(argv[1]);
+	Sales_data total;
+	if (read(input, total))
+	{
+		Sales_data trans;
+		while(read(input, trans))
+		{
+			if (total.isbn() == trans.isbn())
+				total.combine(trans);
+			else
+			{
+				print(cout, total) << endl;
+				total = trans;
+			}
+		}
+		print(cout, total) << endl;
+	}
+	else
+	{
+		cerr << "No data?!" << endl;
+	}
 }
