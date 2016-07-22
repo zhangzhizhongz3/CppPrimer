9cc0b1d763279f620faf214df6c59584c246012a
zzz <zhangzhizhongz3@foxmail.com>
2016-07-22 21:39:55 +0800

    Update ex8_08.cpp

--- ch08/ex8_08.cpp
+++ ch08/ex8_08.cpp
@@ -8,38 +8,38 @@
 //  @Brief  Revise the program from the previous exercise to append its output to its given file.
 //          Run the program on the same output file at least twice to ensure that the data are preserved
 //  @See    ex8_07.cpp
-//  @Run    give a parameter: "../data/book.txt ../data/out.txt"
+//  @Run    give arguments: "E:\book_sales.txt" "E:\zzz.txt"
 
-#include <fstream>
 #include <iostream>
-
-#include "../ch07/ex7_26.h"
+using std::cerr; using std::cin; using std::cout; using std::endl;
+#include <fstream>
 using std::ifstream;
 using std::ofstream;
-using std::endl;
-using std::cerr;
 
-int main(int argc, char** argv)
+#include "ex7_26.h"
+
+int main(int argc, char *argv[])
 {
-    ifstream input(argv[1]);
-    ofstream output(argv[2], ofstream::app);
-
-    Sales_data total;
-    if (read(input, total)) {
-        Sales_data trans;
-        while (read(input, trans)) {
-            if (total.isbn() == trans.isbn())
-                total.combine(trans);
-            else {
-                print(output, total) << endl;
-                total = trans;
-            }
-        }
-        print(output, total) << endl;
-    }
-    else {
-        cerr << "No data?!" << endl;
-    }
-
-    return 0;
+	ifstream input(argv[1]);
+	ofstream output(argv[2], ofstream::app);
+	Sales_data total;
+	if (read(input, total))
+	{
+		Sales_data trans;
+		while(read(input, trans))
+		{
+			if (total.isbn() == trans.isbn())
+				total.combine(trans);
+			else
+			{
+				print(output, total) << endl;
+				total = trans;
+			}
+		}
+		print(output, total) << endl;
+	}
+	else
+	{
+		cerr << "No data?!" << endl;
+	}
 }
