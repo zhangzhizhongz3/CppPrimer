eec6bff5244dddb4dd19529600c60223f0d84914
zzz <zhangzhizhongz3@foxmail.com>
2016-07-23 09:16:27 +0800

    Update ex8_09.cpp

--- ch08/ex8_09.cpp
+++ ch08/ex8_09.cpp
@@ -6,24 +6,26 @@
 //  Copyright (c) 2014 pezy. All rights reserved.
 //
 //  @Brief  Use the function you wrote for the first exercise in § 8.1.2 (p.314)
-//  to print the contents of an istringstream object.
+//          to print the contents of an istringstream object.
 //  @See    Exercise 8.1
 
 #include <iostream>
+#include <string>
 #include <sstream>
-using std::istream;
 
-istream& func(istream& is)
+std::istream &func(std::istream &is)
 {
-    std::string buf;
-    while (is >> buf) std::cout << buf << std::endl;
+    std::string word;
+    while(is>>word)
+    {
+        std::cout<<word<<" ";
+    }
     is.clear();
     return is;
 }
 
 int main()
 {
-    std::istringstream iss("hello");
-    func(iss);
-    return 0;
+   std::istringstream line("zhang zhi zhong");
+   func(line);
 }
