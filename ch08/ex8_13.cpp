e114d5af6799282346b32dc6abeb3e4e9b284087
zzz <zhangzhizhongz3@foxmail.com>
2016-07-23 11:45:15 +0800

    Update ex8_13.cpp

--- ch08/ex8_13.cpp
+++ ch08/ex8_13.cpp
@@ -10,20 +10,20 @@
 //  @See    ex8_11.cpp
 
 #include <iostream>
-#include <sstream>
-#include <fstream>
 #include <string>
 #include <vector>
-
-using std::vector;
-using std::string;
+#include <fstream>
+#include <sstream>
+#include <cctype>
 using std::cin;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::string;
+using std::vector;
+using std::ifstream;
 using std::istringstream;
 using std::ostringstream;
-using std::ifstream;
-using std::cerr;
-using std::cout;
-using std::endl;
 using std::isdigit;
 
 struct PersonInfo {
@@ -31,49 +31,57 @@ struct PersonInfo {
     vector<string> phones;
 };
 
-bool valid(const string& str)
+bool valid(const string &str)
 {
     return isdigit(str[0]);
 }
 
-string format(const string& str)
+string format(const string &str)
 {
-    return str.substr(0, 3) + "-" + str.substr(3, 3) + "-" + str.substr(6);
+    return str.substr(0,3)+"-"+str.substr(3,3)+"-"+str.substr(6);
 }
 
 int main()
 {
-    ifstream ifs("../data/phonenumbers.txt");
-    if (!ifs) {
-        cerr << "no phone numbers?" << endl;
-        return -1;
-    }
-
     string line, word;
     vector<PersonInfo> people;
     istringstream record;
-    while (getline(ifs, line)) {
-        PersonInfo info;
-        record.clear();
-        record.str(line);
-        record >> info.name;
-        while (record >> word) info.phones.push_back(word);
-        people.push_back(info);
+    ifstream input("E:\\zzz.txt");
+    if(input)
+    {
+        while(getline(input, line))
+        {
+            PersonInfo info;
+            record.clear();
+            record.str(line);
+            record>>info.name;
+            while(record>>word)
+                info.phones.push_back(word);
+            people.push_back(info);
+        }
+    }
+    else
+    {
+        cerr<<"no phone numbers?"<<endl;
+        return -1;
     }
 
-    for (const auto& entry : people) {
+    for(const auto &entry:people)
+    {
         ostringstream formatted, badNums;
-        for (const auto& nums : entry.phones)
-            if (!valid(nums))
-                badNums << " " << nums;
+        for(const auto &nums:entry.phones)
+        {
+            if(!valid(nums))
+            {
+                badNums<<" "<<nums;
+            }
             else
-                formatted << " " << format(nums);
-        if (badNums.str().empty())
-            cout << entry.name << " " << formatted.str() << endl;
+                formatted<<" "<<format(nums);
+        }
+        if(badNums.str().empty())
+            cout<<entry.name<<" "<<formatted.str()<<endl;
         else
-            cerr << "input error: " << entry.name << " invalid number(s) "
-                 << badNums.str() << endl;
+            cerr<<"input error: "<<entry.name<<" invalid number(s) "<<badNums.str()<<endl;
     }
 
-    return 0;
 }
