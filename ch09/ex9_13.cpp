2ac43251caebb6f4943afe0050318808c252b429
zzz <zhangzhizhongz3@foxmail.com>
2016-07-25 16:48:54 +0800

    Update ex9_13.cpp

--- ch09/ex9_13.cpp
+++ ch09/ex9_13.cpp
@@ -5,33 +5,36 @@
 //! From a vector<int>?
 //! Write code to check your answers.
 //!
-#include <iostream>
-#include <string>
-#include <vector>
-#include <list>
 
-using std::list;
-using std::vector;
+#include <iostream>
+#include <list>
+#include <vector>
+
 using std::cout;
 using std::endl;
+using std::list;
+using std::vector;
 
 int main()
 {
-    list<int> ilst(5, 4);
-    vector<int> ivc(5, 5);
+    list<int> ilst(5,4);
+    vector<int> ivec(5,5);
 
-    //! from list<int> to vector<double>
-    vector<double> dvc(ilst.begin(), ilst.end());
-    for (auto i : ilst) cout << i;
-    cout << endl;
-    for (auto t : dvc) cout << t;
-    cout << endl;
+    //!from list<int> to vector<double>
+    vector<double> dvec(ilst.begin(), ilst.end());
+    for(auto i:ilst)
+        cout<<i<<" ";
+    cout<<endl;
+    for(auto d:dvec)
+        cout<<d<<" ";
+    cout<<endl;
 
-    //! from vector<int> to vector<double>
-    vector<double> dvc2(ivc.begin(), ivc.end());
-    for (auto i : ivc) cout << i;
-    cout << endl;
-    for (auto t : dvc2) cout << t;
-
-    return 0;
+    //!from vector<int> to vector<double>
+    vector<double> dvec2(ivec.begin(), ivec.end());
+    for(auto i:ivec)
+        cout<<i<<" ";
+    cout<<endl;
+    for(auto d:dvec2)
+        cout<<d<<" ";
+    cout<<endl;
 }
