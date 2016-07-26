f41b7f87eb2432e8dc95ef66db7c08c3f5e95a29
zzz <zhangzhizhongz3@foxmail.com>
2016-07-26 16:12:21 +0800

    Update ex9_16.cpp

--- ch09/ex9_16.cpp
+++ ch09/ex9_16.cpp
@@ -10,14 +10,14 @@
 
 int main()
 {
-    std::list<int> list{1, 2, 3, 4, 5};
-    std::vector<int> vec1{1, 2, 3, 4, 5};
-    std::vector<int> vec2{1, 2, 3, 4};
+    std::list<int> ilst{1,2,3,4,5};
+    std::vector<int> ivec1{1,2,3,4,5};
+    std::vector<int> ivec2{1,2,3,4};
 
-    std::cout << std::boolalpha
-              << (std::vector<int>(list.begin(), list.end()) == vec1)
-              << std::endl;
-    std::cout << std::boolalpha
-              << (std::vector<int>(list.begin(), list.end()) == vec2)
-              << std::endl;
+    std::cout<<std::boolalpha
+             <<(std::vector<int>(ilst.begin(), ilst.end())==ivec1)
+             <<std::endl;
+    std::cout<<std::boolalpha
+             <<(std::vector<int>(ilst.begin(), ilst.end())==ivec2)
+             <<std::endl;
 }
