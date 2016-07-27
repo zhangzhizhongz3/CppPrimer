a9961cc2e9cf5d5bae2cc69d472e589317d87df4
zzz <zhangzhizhongz3@foxmail.com>
2016-07-27 09:43:15 +0800

    Update ex9_18.cpp

--- ch09/ex9_18.cpp
+++ ch09/ex9_18.cpp
@@ -5,28 +5,27 @@
 //  Created by pezy on 12/3/14.
 //  Copyright (c) 2014 pezy. All rights reserved.
 //
-//  @Brief  Write a program to read a sequence of strings from the standard
-//  input into
-//          a deque. Use iterators to write a loop to print the elements in the
-//          deque.
+//  @Brief  Write a program to read a sequence of strings from the standard input into
+//          a deque. Use iterators to write a loop to print the elements in the deque.
 
 #include <iostream>
-#include <string>
 #include <deque>
+#include <string>
 
-using std::string;
-using std::deque;
-using std::cout;
 using std::cin;
+using std::cout;
 using std::endl;
+using std::deque;
+using std::string;
 
 int main()
 {
     deque<string> input;
-    for (string str; cin >> str; input.push_back(str))
+    for(string str; cin>>str; input.push_back(str))
         ;
-    for (auto iter = input.cbegin(); iter != input.cend(); ++iter)
-        cout << *iter << endl;
-
-    return 0;
+    for(auto iter=input.cbegin(); iter!=input.cend(); ++iter)
+    {
+        cout<<*iter<<" ";
+    }
+    cout<<endl;
 }
