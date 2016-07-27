969c03c73c045a948fc5718a99e8fd1a0bc736ad
zzz <zhangzhizhongz3@foxmail.com>
2016-07-27 10:01:22 +0800

    Update ex9_20.cpp

--- ch09/ex9_20.cpp
+++ ch09/ex9_20.cpp
@@ -10,24 +10,26 @@
 //          into the other.
 
 #include <iostream>
-#include <deque>
 #include <list>
-using std::deque;
-using std::list;
-using std::cout;
+#include <deque>
+
 using std::cin;
+using std::cout;
 using std::endl;
+using std::list;
+using std::deque;
 
 int main()
 {
-    list<int> l{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    list<int> ilst{1,2,3,4,5,6,7,8,9,10};
     deque<int> odd, even;
-    for (auto i : l) (i & 0x1 ? odd : even).push_back(i);
+    for(auto i:ilst)
+        ((i & 0x1) ? odd : even).push_back(i);
 
-    for (auto i : odd) cout << i << " ";
-    cout << endl;
-    for (auto i : even) cout << i << " ";
-    cout << endl;
-
-    return 0;
+    for(auto i: odd)
+        cout<<i<<" ";
+    cout<<endl;
+    for(auto i: even)
+        cout<<i<<" ";
+    cout<<endl;
 }
