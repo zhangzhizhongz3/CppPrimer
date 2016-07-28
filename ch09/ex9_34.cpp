f5d15f4b8dd34b8b92147e8195ed86dd62c4d159
zzz <zhangzhizhongz3@foxmail.com>
2016-07-28 19:50:01 +0800

    Update ex9_34.cpp

--- ch09/ex9_34.cpp
+++ ch09/ex9_34.cpp
@@ -1,15 +1,14 @@
 //
-//  ex9_32.cpp
-//  Exercise 9.32
+//  ex9_34.cpp
+//  Exercise 9.34
 //
 //  Created by pezy on 12/3/14.
 //  Copyright (c) 2014 pezy. All rights reserved.
 //
 //  @Brief  Assuming vi is a container of ints that includes even and odd
-//  values,
-//          predict the behavior of the following loop.
-//          After you've analyzed this loop, write a program to test whether
-//          your expectations were correct.
+//          values, predict the behavior of the following loop. After you've 
+//          analyzed this loop, write a program to test whether your expectations 
+//          were correct.
 //
 //			iter = vi.begin();
 //			while (iter != vi.end())
@@ -18,7 +17,7 @@
 //				++iter;
 //
 //  @Answer "infinite loop". Casue the `++iter` out of the `while` loop. But
-//  even through add brackets it is still infinite loop.
+//          even through add brackets it is still infinite loop.
 //          Fixed following.
 
 #include <iostream>
@@ -32,15 +31,18 @@ int main()
 {
     vector<int> vi = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     auto iter = vi.begin();
-    while (iter != vi.end()) {
-        if (*iter % 2) {
+    while (iter != vi.end())
+    {
+        if (*iter % 2)
+        {
             iter = vi.insert(iter, *iter);
             ++iter;
         }
         ++iter;
     }
 
-    for (auto i : vi) cout << i << " ";
-
-    return 0;
+    for (auto i : vi)
+        cout << i << " ";
+    cout << endl;
 }
+
