7dac27e92197c9fd7ea6cfdbeca70c9493811658
zzz <zhangzhizhongz3@foxmail.com>
2016-07-28 16:35:26 +0800

    Update ex9_31_1.cpp

--- ch09/ex9_31_1.cpp
+++ ch09/ex9_31_1.cpp
@@ -8,7 +8,7 @@
 //  @Brief  The program on page 354 to remove even-valued elements and
 //          duplicate odd ones will not work on a list or forward_list. Why?
 //          Revise the program so that it works on these types as well.
-//  @list   iter += 2; -> advance(iter, 2);
+//  @list   ++iter; ++iter; -> advance(iter, 2);
 //
 
 #include <iostream>
