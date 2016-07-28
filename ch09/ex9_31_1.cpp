54cd25c43ebcb07f88def42289eeca05ac1ddf42
zzz <zhangzhizhongz3@foxmail.com>
2016-07-28 16:31:03 +0800

    Update ex9_31_1.cpp

--- ch09/ex9_31_1.cpp
+++ ch09/ex9_31_1.cpp
@@ -14,25 +14,27 @@
 #include <iostream>
 #include <list>
 
-using std::list;
 using std::cout;
 using std::endl;
+using std::list;
 using std::advance;
 
 int main()
 {
-    list<int> vi = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    auto iter = vi.begin();
-    while (iter != vi.end()) {
-        if (*iter % 2) {
-            iter = vi.insert(iter, *iter);
+    list<int> vi={0,1,2,3,4,5,6,7,8,9};
+    auto iter=vi.begin();
+    while(iter!=vi.end())
+    {
+        if(*iter%2)
+        {
+            iter=vi.insert(iter, *iter);
             advance(iter, 2);
         }
         else
-            iter = vi.erase(iter);
+            iter=vi.erase(iter);
     }
 
-    for (auto i : vi) cout << i << " ";
-
-    return 0;
+    for(auto i:vi)
+        cout<<i<<" ";
+    cout<<endl;
 }
