df1776c625d2f7f5daa909d0857929cdd29c563f
zzz <zhangzhizhongz3@foxmail.com>
2016-07-28 16:46:54 +0800

    Update ex9_31_2.cpp

--- ch09/ex9_31_2.cpp
+++ ch09/ex9_31_2.cpp
@@ -9,33 +9,36 @@
 //          duplicate odd ones will not work on a list or forward_list. Why?
 //          Revise the program so that it works on these types as well.
 //  @forward_list   1. vi.insert -> vi.insert_after
-//                  2. iter += 2; -> advance(iter, 2);
+//                  2. ++iter; ++iter; -> advance(iter, 2);
 //                  3. vi.erase -> vi.erase_after
 //                  4. added iterator prev, and edit some detail
 
 #include <iostream>
 #include <forward_list>
 
-using std::forward_list;
 using std::cout;
 using std::endl;
+using std::forward_list;
 using std::advance;
 
 int main()
 {
-    forward_list<int> vi = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    auto iter = vi.begin(), prev = vi.before_begin();
-    while (iter != vi.end()) {
-        if (*iter % 2) {
-            iter = vi.insert_after(prev, *iter);
-            advance(iter, 2);
+    forward_list<int> vi={0,1,2,3,4,5,6,7,8,9};
+    auto prev=vi.before_begin();
+    auto curr=vi.begin();
+    while(curr!=vi.end())
+    {
+        if(*curr%2)
+        {
+            curr=vi.insert_after(prev, *curr);
+            advance(curr, 2);
             advance(prev, 2);
         }
         else
-            iter = vi.erase_after(prev);
+            curr=vi.erase_after(prev);
     }
 
-    for (auto i : vi) cout << i << " ";
-
-    return 0;
+    for(auto i:vi)
+        cout<<i<<" ";
+    cout<<endl;
 }
