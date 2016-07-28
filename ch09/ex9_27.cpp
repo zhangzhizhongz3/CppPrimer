aa52759d224b9c6713a23590224ff1686e878043
zzz <zhangzhizhongz3@foxmail.com>
2016-07-28 12:06:32 +0800

    Update ex9_27.cpp

--- ch09/ex9_27.cpp
+++ ch09/ex9_27.cpp
@@ -10,8 +10,7 @@ int main()
 {
 
 	forward_list<int> flst{0,1,2,3,4,5,6,7,8,9};
-	auto prev=flst.before_begin();
-	auto curr=flst.begin();
+
 	for(auto prev=flst.before_begin(), curr=flst.begin(); curr!=flst.end(); )
         if(*curr & 0x1)
             curr=flst.erase_after(prev);
