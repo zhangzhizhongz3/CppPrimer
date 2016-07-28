2d2d559c4df13af8e9a756191dd8a550cf24c651
zzz <zhangzhizhongz3@foxmail.com>
2016-07-28 19:26:33 +0800

    Update ex9_33.cpp

--- ch09/ex9_33.cpp
+++ ch09/ex9_33.cpp
@@ -10,8 +10,8 @@
 //          Write a program that omits this assignment to see if your
 //          expectation was correct.
 //  @Answer crash, cause the iterator is invalid after insert. Because
-//  Iterators, pointers, and references to a
-//          vector or string are invalid if the container was reallocated.
+//          iterators, pointers, and references to a vector or 
+//          string are invalid if the container was reallocated.
 
 #include <iostream>
 #include <vector>
@@ -22,15 +22,16 @@ using std::endl;
 
 int main()
 {
-    vector<int> v{1, 2, 3, 4, 5, 6, 7, 8, 9};
-    auto begin = v.begin();
-    while (begin != v.end()) {
+    vector<int> v{1,2,3,4,5,6,7,8,9};
+    auto begin=v.begin();
+    while(begin!=v.end())
+    {
         ++begin;
-        /*begin = */ v.insert(begin, 42);
+        /*begin=*/v.insert(begin, 42);
         ++begin;
     }
-
-    for (auto i : v) cout << i << " ";
-
-    return 0;
+    
+    for(auto i:v)
+        cout<<i<<" ";
+    cout<<endl;
 }
