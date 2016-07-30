a474f304760c36e5ab2c3513c6f6d52699880680
zzz <zhangzhizhongz3@foxmail.com>
2016-07-30 11:29:32 +0800

    Update ex9_43.cpp

--- ch09/ex9_43.cpp
+++ ch09/ex9_43.cpp
@@ -11,53 +11,55 @@
 //          Test your function by using it to replace common abbreviations,
 //          such as "tho" by "though" and "thru" by "through".
 
-#include <string>
-using std::string;
-
 #include <iostream>
+#include <iterator>
+#include <string>
+using namespace std;
 
-void Replace(string& s, const string& oldVal, const string& newVal)
+void Replace(string &s, const string &oldVal, const string &newVal)
 {
-    for (auto beg = s.begin(); beg != s.end(); ++beg) {
-        if (*beg != oldVal.front()) continue;
-        if (std::distance(beg, s.end()) <
-            std::distance(oldVal.begin(), oldVal.end()))
+    for(auto beg=s.begin(); beg!=s.end(); ++beg)
+    {
+        if(*beg!=oldVal.front())
+            continue;
+        if(distance(beg, s.end())<distance(oldVal.begin(), oldVal.end()))
             break;
-        if (string{beg, beg + oldVal.size()} == oldVal) {
-            auto pos = std::distance(s.begin(), beg);
-            s.erase(beg, beg + oldVal.size());
+        if(string(beg, beg+oldVal.size())==oldVal)
+        {
+            auto pos=distance(s.begin(), beg);
+            s.erase(beg, beg+oldVal.size());
             s.insert(beg, newVal.cbegin(), newVal.cend());
-            beg = std::next(s.begin(), pos + newVal.size() - 1);
+            beg=next(s.begin(), pos+newVal.size()-1);
         }
     }
 }
 
 int main()
 {
-    {
-        string str{"To drive straight thru is a foolish, tho courageous act."};
-        Replace(str, "thru", "through");
-        Replace(str, "tho", "though");
-        std::cout << str << std::endl;
-    }
-    {
-        string str{
-            "To drive straight thruthru is a foolish, thotho courageous act."};
-        Replace(str, "thru", "through");
-        Replace(str, "tho", "though");
-        std::cout << str << std::endl;
-    }
-    {
-        string str{"To drive straight thru is a foolish, tho courageous act."};
-        Replace(str, "thru", "thruthru");
-        Replace(str, "tho", "though");
-        std::cout << str << std::endl;
-    }
-    {
-        string str{"my world is a big world"};
-        Replace(str, "world",
-                "worldddddddddddddddddddddddddddddddddddddddddddddddd");
-        std::cout << str << std::endl;
-    }
-    return 0;
+        {
+            string str{"To drive straight thru is a foolish, tho courageous act."};
+            Replace(str, "thru", "through");
+            Replace(str, "tho", "though");
+            cout << str << endl;
+        }
+
+        {
+            string str{"To drive straight thruthru is a foolish, thotho courageous act."};
+            Replace(str, "thru", "through");
+            Replace(str, "tho", "though");
+            cout << str << endl;
+        }
+
+        {
+            string str{"To drive straight thru is a foolish, tho courageous act."};
+            Replace(str, "thru", "thruthru");
+            Replace(str, "tho", "though");
+            cout << str << endl;
+        }
+
+        {
+            string str{"my world is a big world"};
+            Replace(str, "world", "worldddddddddddddddddddddddddddddddddddddddddddddddd");
+            cout << str << endl;
+        }
 }
