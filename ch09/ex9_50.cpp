5e2098164d064a801e1a735a6a56e48b0bb90101
zzz <zhangzhizhongz3@foxmail.com>
2016-07-31 10:13:45 +0800

    Update ex9_50.cpp

--- ch09/ex9_50.cpp
+++ ch09/ex9_50.cpp
@@ -1,36 +1,36 @@
 //! @Yue Wang
 //!
 //! Exercise 9.50:
-//! Write a program to process a vector<string>s whose elements represent
-//! integral values.
+//! Write a program to process a vector<string> whose elements represent integral values.
 //! Produce the sum of all the elements in that vector.
-//! Change the program so that it sums of strings that represent floating-point
-//! values.
+//! Change the program so that it sums of strings that represent floating-point values.
 //!
 
 #include <iostream>
 #include <string>
 #include <vector>
+using namespace std;
 
-int sum_for_int(const std::vector<std::string> &v)
+int sum_for_int(const vector<string> &svec)
 {
-    int sum = 0;
-    for (auto const& s : v) sum += std::stoi(s);
+    int sum=0;
+    for(const auto &s : svec)
+        sum+=stoi(s);
     return sum;
+
 }
 
-float sum_for_float(const std::vector<std::string> &v)
+float sum_for_float(const vector<string> &svec)
 {
-    float sum = 0.0;
-    for (auto const& s : v) sum += std::stof(s);
+    float sum=0.0;
+    for(const auto &s:svec)
+        sum+=stof(s);
     return sum;
 }
 
 int main()
 {
-    std::vector<std::string> v = {"1", "2", "3", "4.5"};
-    std::cout << sum_for_int(v) << std::endl;
-    std::cout << sum_for_float(v) << std::endl;
-
-    return 0;
+    vector<string> svec{"1", "2", "3", "4.5"};
+    cout<<sum_for_int(svec)<<endl;
+    cout<<sum_for_float(svec)<<endl;
 }
