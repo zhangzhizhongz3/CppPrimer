0f49c9b2af88463ef4977ab0d8d2620c30135d86
zzz <zhangzhizhongz3@foxmail.com>
2016-08-03 10:19:14 +0800

    Update ex10_05.cpp

--- ch10/ex10_05.cpp
+++ ch10/ex10_05.cpp
@@ -6,28 +6,29 @@
 //  Copyright (c) 2014 pezy. All rights reserved.
 //
 //  @Brief  In the call to equal on rosters, what would happen if both rosters
-//  held C-style strings, rather than library strings?
+//          held C-style strings, rather than library strings?
 //  @Answer It's not quite the same as `std::string`
-//  Maybe the function 'equal' return true when you make a comparison between 
-//  two c-style strings containers. Nonetheless, we need to keep in mind that 
-//  when it comes to comparison of c-style strings, we need to use 'strcmp' but 
-//  not simply relational operators, for using relational operators is just 
-//  comparison between the address of two c-style strings but not their values.
+//          Maybe the function 'equal' return true when you make a comparison between 
+//          two C-style strings containers. Nonetheless, we need to keep in mind that 
+//          when it comes to comparison of C-style strings, we need to use 'strcmp' but 
+//          not simply relational operators, for using relational operators is just 
+//          comparison between the address of two C-style strings but not their values.
 
 
-#include <algorithm>
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <list>
 
+using namespace std;
+
 int main()
 {
-    char c1[10] = "eipi10";
-    char c2[10] = "eipi10";
-    std::vector<char*> roster1{c1};
-    std::list<char*> roster2{c2};
-    std::cout << std::equal(roster1.cbegin(), roster1.cend(), roster2.cbegin());
-    return 0;
+    char c1[10]="zhang";
+    char c2[10]="zhang";
+    vector<char*> roster1{c1};
+    list<char*> roster2{c2};
+    cout<<equal(roster1.cbegin(), roster1.cend(), roster2.cbegin())<<endl;
 }
 
 // clang 3.5.0
