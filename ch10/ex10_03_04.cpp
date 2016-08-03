53c4301625e52606807d9421108cafa7cdcde992
zzz <zhangzhizhongz3@foxmail.com>
2016-08-03 09:58:30 +0800

    Update ex10_03_04.cpp

--- ch10/ex10_03_04.cpp
+++ ch10/ex10_03_04.cpp
@@ -8,41 +8,37 @@
 //! Exercise 10.4:
 //! Assuming v is a vector<double>, what, if anything,
 //! is wrong with calling accumulate(v.cbegin(), v.cend(), 0)?
-//  Check below.
+//!  Check below:
 //!
 
 #include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
 #include <numeric>
+#include <vector>
+
+using namespace std;
 
 int main()
 {
     //! Exercise 10.3
-    std::vector<int> v = {1, 2, 3, 4};
-    std::cout << "ex 10.03: " << std::accumulate(v.cbegin(), v.cend(), 0)
-              << std::endl;
-
+    vector<int> ivec={1, 2, 3, 4};
+    cout<<"ex 10.03: "<<accumulate(ivec.cbegin(), ivec.cend(), 0)<<endl;
+    
     //! Exercise 10.4
-    std::vector<double> vd = {1.1, 0.5, 3.3};
-    std::cout << "ex 10.04: " << std::accumulate(vd.cbegin(), vd.cend(), 0)
-              << std::endl; //!   ^<-- note here.
+    vector<double> dvec={1.1, 0.5, 3.3};
+    cout<<"ex 10.04: "<<accumulate(dvec.cbegin(), dvec.cend(), 0)<<endl;    //! ^<--note here
     //! @attention
-    //!
-    //! The ouput is 4 rather than 4.9 as expected.
-    //! The reason is std::accumulate is a template function. The third
+    //! The output is 4 rather than 4.9 as expected.
+    //! The reason is accumulate is a template function. The third
     //! parameter is _Tp __init
-    //! When "0" , an integer, had been specified here, the compiler deduced _Tp
-    //! as
-    //! interger.As a result ,when the following statments were being excuted :
+    //! When "0", an integer, had been specified here, the compiler
+    //! deduced _Tp as integer. As a result, when the following statements
+    //! were being executed:
     //  for (; __first != __last; ++__first)
-    //	__init = __init + *__first;
+    //  __init = __init + *__first;
     //  return __init;
     //! all calculation would be converted to integer.
-
-    return 0;
 }
+
 //! output
 //!
 // ex 10.03: 10
