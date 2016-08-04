2e134aa9428a865ef28e43def4eda9c7e309d06e
zzz <zhangzhizhongz3@foxmail.com>
2016-08-04 16:15:52 +0800

    Update ex10_12.cpp

--- ch10/ex10_12.cpp
+++ ch10/ex10_12.cpp
@@ -1,33 +1,32 @@
 //! @Yue Wang
 //! Exercise 10.12:
-//! Write a function named compareIsbn that compares the isbn() members of two
+//! Write a function named compareIsbn that compares the isbn members of two
 //! Sales_data objects.
 //! Use that function to sort a vector that holds Sales_data objects.
 //!
 
 #include <iostream>
-#include <string>
 #include <vector>
 #include <algorithm>
-#include <numeric>
-#include "../ch07/ex7_26.h" // Sales_data class.
+#include "Sales_data.h" //Sales_data class
 
-inline bool compareIsbn(const Sales_data& sd1, const Sales_data& sd2)
+using namespace std;
+
+bool compareIsbn(const Sales_data &lhs, const Sales_data &rhs)
 {
-    return sd1.isbn().size() < sd2.isbn().size();
+    return lhs.isbn().size()<rhs.isbn().size();
 }
 
 int main()
 {
-    Sales_data d1("aa"), d2("aaaa"), d3("aaa"), d4("z"), d5("aaaaz");
-    std::vector<Sales_data> v{d1, d2, d3, d4, d5};
+    Sales_data d1{"aa"}, d2{"aaaa"}, d3{"aaa"}, d4{"z"}, d5{"aaaaz"};
+    vector<Sales_data> svec{d1, d2, d3, d4, d5};
 
-    //! @note   the elements the iterators pointing to
-    //!         must match the parameters of the predicate.
-    std::sort(v.begin(), v.end(), compareIsbn);
+    // @note the elements the iterators pointing to
+    //       must match the parameters of the predicate
+    sort(svec.begin(), svec.end(), compareIsbn);
 
-    for (const auto& element : v) std::cout << element.isbn() << " ";
-    std::cout << std::endl;
-
-    return 0;
+    for(const auto &element : svec)
+        cout<<element.isbn()<<" ";
+    cout<<endl;
 }
