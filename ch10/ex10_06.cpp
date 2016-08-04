f79b1520ab8a70fe8e823507ca0b76184be201aa
zzz <zhangzhizhongz3@foxmail.com>
2016-08-04 09:09:08 +0800

    Update ex10_06.cpp

--- ch10/ex10_06.cpp
+++ ch10/ex10_06.cpp
@@ -11,9 +11,9 @@
 #include <vector>
 #include <algorithm>
 
-using std::vector;
 using std::cout;
 using std::endl;
+using std::vector;
 using std::fill_n;
 
 int main()
@@ -21,8 +21,9 @@ int main()
     vector<int> vec{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     fill_n(vec.begin(), vec.size(), 0);
 
-    for (auto i : vec) cout << i << " ";
-    cout << endl;
+    for(const auto &i : vec)
+        cout<<i<<" ";
+    cout<<endl;
 }
 
 //  @Output
