e16b8450b29b9670298855b19ab953256896a96b
zzz <zhangzhizhongz3@foxmail.com>
2016-08-04 10:16:47 +0800

    Update ex10_07.cpp

--- ch10/ex10_07.cpp
+++ ch10/ex10_07.cpp
@@ -13,17 +13,14 @@
 #include <list>
 #include <algorithm>
 
-using std::vector;
-using std::cout;
-using std::endl;
-using std::list;
-using std::cin;
-using std::fill_n;
+using namespace std;
 
-template <typename Sequence> void print(Sequence const& seq)
+template <typename Sequence>
+void print(const Sequence &seq)
 {
-    for (const auto& i : seq) cout << i << " ";
-    cout << endl;
+    for(const auto &i:seq)
+        cout<<i<<" ";
+    cout<<endl;
 }
 
 int main()
@@ -32,25 +29,24 @@ int main()
     vector<int> vec;
     list<int> lst;
     int i;
-    while (cin >> i) lst.push_back(i);
+    while(cin>>i)
+        lst.push_back(i);
     vec.resize(lst.size());
     // ^ Fixed: added this statement
-    // Cause Algorithms that write to a destination iterator assume
-    // the destination is large enough to hold the number of elements being
-    // written.
+    // Cause Algorithms that write to a destination iterator assume the
+    // destination is large enough to hold the number of elements being written.
     copy(lst.cbegin(), lst.cend(), vec.begin());
-
     // another way to fix bug
-    // copy(lst.cbegin(), lst.cend(), back_inserter(vec));
+    //copy(lst.cbegin(), lst.cend(), back_inserter(vec));
 
     // (b)
-    vector<int> v;
-    v.reserve(10);
-    fill_n(v.begin(), 10, 0);
-    // ^ (b)No error, but not any sense. v.size() still equal zero.
-    // Fixed: 1. use `v.resize(10);`
-    //    or  2. use `fill_n(std::back_inserter(v), 10, 0)`
+    vector<int> vec2;
+    vec2.reserve(10);
+    fill_n(vec2.begin(), 10, 0);
+    // No error, but not any sense. vec2.size() still equals zero.
+    // ^ Fixed: 1. use 'vec2.resize(10);'
+    //       or 2. use 'fill_n(back_inserter(vec2), 10, 0);'
 
-    print(v);
     print(vec);
+    print(vec2);
 }
