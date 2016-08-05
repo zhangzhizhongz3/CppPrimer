570fab4a7603a83963df15bd36c50ca2efced6a1
zzz <zhangzhizhongz3@foxmail.com>
2016-08-05 20:49:28 +0800

    Update ex10_16.cpp

--- ch10/ex10_16.cpp
+++ ch10/ex10_16.cpp
@@ -3,53 +3,47 @@
 //! @date   02.12.2014
 //!
 //! Exercise 10.16:
-//! Write your own version of the biggies function using lambdas.
+//! Write your own version of the biggies function using lambda.
 //!
 
 #include <iostream>
-#include <string>
 #include <vector>
+#include <string>
 #include <algorithm>
 
+using namespace std;
+
 //! from ex 10.9
-void elimdups(std::vector<std::string>& vs)
+void elimdups(vector<string> &svec)
 {
-    std::sort(vs.begin(), vs.end());
-    auto new_end = std::unique(vs.begin(), vs.end());
-    vs.erase(new_end, vs.end());
+    sort(svec.begin(), svec.end());
+    auto new_end=unique(svec.begin(), svec.end());
+    svec.erase(new_end, svec.end());
 }
 
-void biggies(std::vector<std::string>& vs, std::size_t sz)
+void biggies(vector<string> &words, vector<string>::size_type sz)
 {
-    using std::string;
+    elimdups(words);
 
-    elimdups(vs);
-
-    //! sort by size, but maintain alphabetical order for same size.
-    std::stable_sort(vs.begin(), vs.end(),
-                     [](string const& lhs, string const& rhs) {
-                         return lhs.size() < rhs.size();
-                     });
+    //! sort by size, but maintain alphabetical order for same size
+    stable_sort(words.begin(), words.end(), [](const string &lhs, const string &rhs) {return lhs.size()<rhs.size();});
 
     //! get an iterator to the first one whose size() is >= sz
-    auto wc = std::find_if(vs.begin(), vs.end(),
-                           [sz](string const& s) { return s.size() >= sz; });
+    auto wc=find_if(words.begin(), words.end(), [sz](const string &s) {return s.size()>=sz;});
 
     //! print the biggies
-    std::for_each(wc, vs.end(), [](const string& s) { std::cout << s << " "; });
+    for_each(wc, words.end(), [](const string &s) {cout<<s<<" ";});
 }
 
 int main()
 {
     //! ex10.16
-    std::vector<std::string> v{"1234", "1234", "1234", "hi~",
-                               "alan", "alan", "cp"};
-    std::cout << "ex10.16: ";
-    biggies(v, 3);
-    std::cout << std::endl;
-
-    return 0;
+    vector<string> svec{"1234", "1234", "1234", "hi~", "alan","alan", "cp"};
+    cout<<"ex10.16: ";
+    biggies(svec, 3);
+    cout<<endl;
 }
+
 //! output :
 //!
 // ex10.16: hi~ 1234 alan 
