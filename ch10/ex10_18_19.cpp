23abe991c5b7fba2c9a361ccd3b2250bb50849a1
zzz <zhangzhizhongz3@foxmail.com>
2016-08-05 21:10:45 +0800

    Update ex10_18_19.cpp

--- ch10/ex10_18_19.cpp
+++ ch10/ex10_18_19.cpp
@@ -7,7 +7,7 @@
 //!
 //! Exercise 10.19:
 //! Rewrite the previous exercise to use stable_partition, which like
-//! stable_sort maintains the original element order in the paritioned
+//! stable_sort maintains the original element order in the partitioned
 //! sequence.
 //!
 
