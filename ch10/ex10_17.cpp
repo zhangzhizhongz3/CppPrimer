115366f5f86f6ca554b56626513372b61dc53c6b
zzz <zhangzhizhongz3@foxmail.com>
2016-08-05 20:59:36 +0800

    Update ex10_17.cpp

--- ch10/ex10_17.cpp
+++ ch10/ex10_17.cpp
@@ -1,29 +1,26 @@
 //! @pezy
 //!
 //! Exercise 10.17
-//! Rewrite exercise 10.12 from ¡ì 10.3.1 (p. 387)
+//! Rewrite exercise 10.12 from Â§ 10.3.1 (p. 387)
 //! to use a lambda in the call to sort instead of the compareIsbn function.
 //!
 //! @See 7.26, 10.12
 
 #include <iostream>
-#include <string>
 #include <vector>
 #include <algorithm>
-#include "../ch07/ex7_26.h" // Sales_data class.
+#include "Sales_data.h"
+
+using namespace std;
 
 int main()
 {
-    Sales_data d1("aa"), d2("aaaa"), d3("aaa"), d4("z"), d5("aaaaz");
-    std::vector<Sales_data> v{d1, d2, d3, d4, d5};
+    Sales_data d1{"aa"}, d2{"aaaa"}, d3{"aaa"}, d4{"z"}, d5{"aaaaz"};
+    vector<Sales_data> svec{d1, d2, d3, d4, d5};
 
-    std::sort(v.begin(), v.end(),
-              [](const Sales_data& sd1, const Sales_data& sd2) {
-                  return sd1.isbn().size() < sd2.isbn().size();
-              });
+    sort(svec.begin(), svec.end(), [](const Sales_data &lhs, const Sales_data &rhs) {return lhs.isbn().size()<rhs.isbn().size();});
 
-    for (const auto& element : v) std::cout << element.isbn() << " ";
-    std::cout << std::endl;
-
-    return 0;
+    for(const auto &element : svec)
+        cout<<element.isbn()<<" ";
+    cout<<endl;
 }
