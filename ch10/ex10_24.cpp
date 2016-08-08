dc12bf8194cd0a8ed1e3e7f27b0ceba66a2e141f
zzz <zhangzhizhongz3@foxmail.com>
2016-08-08 12:11:50 +0800

    Update ex10_24.cpp

--- ch10/ex10_24.cpp
+++ ch10/ex10_24.cpp
@@ -3,46 +3,51 @@
 //!
 //! Exercise 10.24:
 //! Use bind and check_size to find the first element in a vector of ints that
-//! has a value greater
-//! than the length of a specified string value.
+//! has a value greater than the length of a specified string value.
 //!
 //  Discussion over this exercise on StackOverflow
 //  http://stackoverflow.com/questions/20539406/what-type-does-stdfind-if-not-return
 //!
 
 #include <iostream>
-#include <string>
 #include <vector>
+#include <string>
 #include <algorithm>
 #include <functional>
+using namespace std;
+using namespace std::placeholders;
 
-using std::cout;
-using std::endl;
-using std::string;
-using std::vector;
-using std::find_if;
-using std::bind;
-
-inline bool check_size(const string& s, string::size_type sz)
+bool check_size(const string &s, string::size_type sz)
 {
-    return s.size() < sz;
+    return s.size()<sz;
 }
 
-inline vector<int>::const_iterator find_first_bigger(const vector<int>& v,
-                                                     const string& s)
+vector<int>::const_iterator find_first_bigger(const vector<int> &v, const string &s)
 {
-    return find_if(v.cbegin(), v.cend(),
-                   bind(check_size, s, std::placeholders::_1));
+    return find_if(v.cbegin(), v.cend(), bind(check_size, s, _1));
 }
 
 int main()
 {
-    vector<int> v{1, 2, 3, 4, 5, 6, 7};
+    vector<int> v{3,2,5,0,6,5,3,9,5};
     string s("test");
-    cout << *find_first_bigger(v, s) << endl;
-
-    return 0;
+    cout<<*find_first_bigger(v, s)<<endl;
 }
-//! output;
-//!
-// 5
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+using namespace std;
+using namespace std::placeholders;
+
+bool check_size(const string &s, int i)
+{
+    return s.size()<i;
+}
+
+int main()
+{
+    vector<int> v{3,2,5,0,3,5,3,9,5};
+    cout<<*find_if(v.cbegin(), v.cend(), bind(check_size, "test", _1))<<endl;
+}
