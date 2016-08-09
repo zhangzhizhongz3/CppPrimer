442da09bbf16d7d2a3448c5a7b2ecc989ac983dc
zzz <zhangzhizhongz3@foxmail.com>
2016-08-09 20:26:26 +0800

    Update ex10_33.cpp

--- ch10/ex10_33.cpp
+++ ch10/ex10_33.cpp
@@ -7,31 +7,24 @@
 //
 //  Write a program that takes the names of an input file and two output files.
 //  The input file should hold integers. Using an istream_iterator read the
-//  input file.
-//  Using ostream_iterators, write the odd numbers into the first output file.
-//  Each value should be followed by a space.Write the even numbers into the
-//  second file.
-//  Each of these values should be placed on a separate line.
+//  input file. Using ostream_iterators, write the odd numbers into the first
+//  output file. Each value should be followed by a space. Write the even numbers 
+//  into the second output file. Each of these values should be placed on a separate line.
 //
-//  Run: ./a.out "../data/input.txt" "../data/odd.txt" "../data/even.txt"
 
-#include <fstream>
 #include <iterator>
+#include <fstream>
 #include <algorithm>
+using namespace std;
 
-int main(int argc, char** argv)
+int main(int argc, char *argv[])
 {
-    if (argc != 4) return -1;
+    if(argc!=4)
+        return -1;
+    ifstream ifs(argv[1]);
+    ofstream ofs_odd(argv[2]), ofs_even(argv[3]);
 
-    std::ifstream ifs(argv[1]);
-    std::ofstream ofs_odd(argv[2]), ofs_even(argv[3]);
-
-    std::istream_iterator<int> in(ifs), in_eof;
-    std::ostream_iterator<int> out_odd(ofs_odd, " "), out_even(ofs_even, "\n");
-
-    std::for_each(in, in_eof, [&out_odd, &out_even](const int i) {
-        *(i & 0x1 ? out_odd : out_even)++ = i;
-    });
-
-    return 0;
+    istream_iterator<int> in(ifs), in_eof;
+    ostream_iterator<int> out_odd(ofs_odd, " "), out_even(ofs_even, "\n");
+    for_each(in, in_eof, [&out_odd, &out_even](const int i) {(i & 0x1 ? out_odd : out_even)=i;});
 }
