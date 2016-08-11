86710e9430c61ce77603e12a52d521d117208f1d
zzz <zhangzhizhongz3@foxmail.com>
2016-08-11 22:54:42 +0800

    Update ex11_14.cpp

--- ch11/ex11_14.cpp
+++ ch11/ex11_14.cpp
@@ -1,6 +1,6 @@
 //! @Yue Wang
 //! Exercise 11.14:
-//! Extend the map of children to their family name that you wrote for the
+//! Extend the map of children's names to their family name that you wrote for the
 //! exercises in § 11.2.1 (p. 424) by having the vector store a pair that
 //! holds a child’s name and birthday.
 //!
