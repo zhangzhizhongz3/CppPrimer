d1720982dc5ed90df470a105cacf1ef29b5d867f
zzz <zhangzhizhongz3@foxmail.com>
2016-08-11 15:41:08 +0800

    Update ex11_7.cpp

--- ch11/ex11_7.cpp
+++ ch11/ex11_7.cpp
@@ -4,48 +4,40 @@
 //! Define a map for which the key is the family’s last name and
 //! the value is a vector of the children’s names. Write code to
 //! add new families and to add new children to an existing family.
-//  Discussion on Stack Overflow:
-//  http://stackoverflow.com/questions/20608365/is-it-possible-to-code-this-waywhilelambda
 //!
 
 #include <iostream>
 #include <map>
+#include <vector>
 #include <string>
-#include <algorithm>
+
+using namespace std;
 
 int main()
 {
-    std::map<std::string, std::vector<std::string>> famls;
-
-    std::string lastName, chldName;
+    map<string, vector<string>> famls;
+    string lastName, chldName;
 
     //! while(lambda)
-    //! go to the discussions on stack overfow for more.
-    while ([&]() -> bool {
-        std::cout << "Please enter last name:\n";
-
-        return std::cin >> lastName && lastName != "@q";
-    }())
-    //!  ^^
-    //! the () used here is to call the lambda ,otherwise it does not work
-    //! go to the post on stack overflow for more detail.
-
+    cin.clear();
+    while([&]() ->bool {cout<<"Please enter last name:\n"; return cin>>lastName&&lastName!="@q";}())    //the () used here is to call the lambda, otherwise it does not work
     {
-        std::cout << "PLZ Enter children's name:\n";
-        while (std::cin >> chldName && chldName != "@q") {
-            //! add new items into the vector
+        cout<<"Please enter children's names:\n";
+        cin.clear();
+        while(cin>>chldName&&chldName!="@q")
+        {
+            //! add new items into vector
             famls[lastName].push_back(chldName);
         }
     }
 
-    //! iterate through the map.
-    for (auto e : famls) {
-        std::cout << e.first << ":\n";
-
-        //! iterate through the vector.
-        for (auto c : e.second) std::cout << c << " ";
-        std::cout << "\n";
+    //! iterate through the map
+    for(const auto &e:famls)
+    {
+        cout<<e.first<<":\n" ;
+        //iterate through the vector
+        for(const auto &c: e.second)
+            cout<<c<<" ";
+        cout<<endl;
     }
-
-    return 0;
 }
