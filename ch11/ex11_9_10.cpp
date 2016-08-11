30a8a3b0292a7fb11bbb1c994d3db2a6ab49db7b
zzz <zhangzhizhongz3@foxmail.com>
2016-08-11 17:14:20 +0800

    Update ex11_9_10.cpp

--- ch11/ex11_9_10.cpp
+++ ch11/ex11_9_10.cpp
@@ -10,29 +10,31 @@
 //! In each case, if not, why not?
 //  vector<int>::iterator to int is ok ,because < is defined
 //  list<int>::iterator to int is not ok,as no < is defined.
-#include <iostream>
+
 #include <map>
 #include <string>
-#include <algorithm>
+#include <vector>
 #include <list>
+#include <cstddef>
+#include <algorithm>
+
+using namespace std;
 
 int main()
 {
     //! ex 11.9
-    std::map<std::string, std::list<std::size_t>> m;
+    map<string, list<size_t>> m;
 
     //! ex 11.10
-    //! can be declared.
-    std::map<std::vector<int>::iterator, int> mv;
-    std::map<std::list<int>::iterator, int> ml;
+    //! can be declared
+    map<vector<int>::iterator, int> mv;
+    map<list<int>::iterator, int> ml;
 
-    std::vector<int> vi;
-    mv.insert(std::pair<std::vector<int>::iterator, int>(vi.begin(), 0));
+    vector<int> vi;
+    mv.insert(pair<vector<int>::iterator, int>(vi.begin(), 0));
 
     //! but when using this one the compiler complained that
-    //! error: no match for 'operator<' in '__x < __y'
-    std::list<int> li;
-    ml.insert(std::pair<std::list<int>::iterator, int>(li.begin(), 0));
-
-    return 0;
+    //! error: no match for 'operator<' in '_x < _y'
+    list<int> li;
+    ml.insert(pair<list<int>::iterator, int>(li.begin(), 0));
 }
