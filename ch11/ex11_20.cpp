c57c146b029a04f4296ba0b8bf18ee65a543517c
zzz <zhangzhizhongz3@foxmail.com>
2016-08-12 16:11:32 +0800

    Update ex11_20.cpp

--- ch11/ex11_20.cpp
+++ ch11/ex11_20.cpp
@@ -1,7 +1,7 @@
 //! @Alan @pezy
 //!
 //! Exercise 11.20:
-//! Rewrite the word-counting program from § 11.1 (p. 421) to use insert instead
+//! Rewrite the word counting program from § 11.1 (p. 421) to use insert instead
 //! of subscripting. Which program do you think is easier to write and read?
 //! Explain your reasoning.
 //!
@@ -9,20 +9,24 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <cstddef>
 
-using std::string;
+using namespace std;
 
 int main()
 {
-    std::map<string, size_t> word_count;
-    string word;
-    while (std::cin >> word) {
-        auto ret = word_count.insert({word, 1});
-        if (!ret.second) ++ret.first->second;
-    }
+   map<string, size_t> word_count;
+   string word;
+   while(cin>>word)
+   {
+       auto ret=word_count.insert({word, 1});
+       if(!ret.second)
+            ++ret.first->second;
+   }
 
-    //! print the content of the map.
-    for (const auto& w : word_count)
-        std::cout << w.first << " " << w.second
-                  << ((w.second > 1) ? " times" : " time") << std::endl;
+   //! print the content of the map
+   for(const auto &w:word_count)
+   {
+       cout<<w.first<<" "<<w.second<<((w.second>1)?" times":" time")<<endl;
+   }
 }
