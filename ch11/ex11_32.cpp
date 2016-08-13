7bfc0328d74a4703bfc6725da4a1bbfad860faf9
zzz <zhangzhizhongz3@foxmail.com>
2016-08-13 16:27:53 +0800

    Update ex11_32.cpp

--- ch11/ex11_32.cpp
+++ ch11/ex11_32.cpp
@@ -8,24 +8,24 @@
 //  Using the multimap from the previous exercise, write a program to print the
 //  list of **authors and their works** alphabetically.
 
+#include <iostream>
 #include <map>
 #include <set>
 #include <string>
-#include <iostream>
 
-using std::string;
+using namespace std;
 
 int main()
 {
-    std::multimap<string, string> authors{
-        {"alan", "DMA"}, {"pezy", "LeetCode"}, {"alan", "CLRS"},
-        {"wang", "FTP"}, {"pezy", "CP5"},      {"wang", "CPP-Concurrency"}};
-    std::map<string, std::multiset<string>> order_authors;
-    for (const auto& author : authors)
+    multimap<string, string> authors={{"alan", "DMA"}, {"pezy", "LeetCode"}, {"alan", "CLRS"}, {"wang", "FTP"}, {"pezy", "CP5"}, {"wang", "CPP-Concurrency"}};
+    map<string, multiset<string>> order_authors;
+    for(const auto &author:authors)
         order_authors[author.first].insert(author.second);
-    for (const auto& author : order_authors) {
-        std::cout << author.first << ": ";
-        for (const auto& work : author.second) std::cout << work << " ";
-        std::cout << std::endl;
+    for(const auto &author:order_authors)
+    {
+        cout<<author.first<<": ";
+        for(const auto &work:author.second)
+            cout<<work<<" ";
+        cout<<endl;
     }
 }
