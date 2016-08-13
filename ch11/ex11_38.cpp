de96e5c4b7c5e106ad387d266374a909978ca50b
zzz <zhangzhizhongz3@foxmail.com>
2016-08-13 18:08:45 +0800

    Update ex11_38.cpp

--- ch11/ex11_38.cpp
+++ ch11/ex11_38.cpp
@@ -5,53 +5,50 @@
 //  Created by pezy on 12/18/14.
 //  Copyright (c) 2014 pezy. All rights reserved.
 //
-//  Rewrite the word-counting (11.1, p. 421) and word-transformation (11.3.6, p.
-//  440) programs to use an unordered_map.
+//  Rewrite the word counting (11.1, p. 421) and word transformation (11.3.6, p. 440) programs to use an unordered_map.
 
-#include <unordered_map>
-#include <set>
-#include <string>
 #include <iostream>
+#include <string>
+#include <cstddef>
 #include <fstream>
 #include <sstream>
+#include <unordered_map>
 
-using std::string;
+using namespace std;
 
 void wordCounting()
 {
-    std::unordered_map<string, size_t> word_count;
-    for (string word; std::cin >> word; ++word_count[word])
+    unordered_map<string, size_t> word_count;
+    for(string word; cin>>word; ++word_count[word])
         ;
-    for (const auto& w : word_count)
-        std::cout << w.first << " occurs " << w.second
-                  << (w.second > 1 ? "times" : "time") << std::endl;
+    for(const auto &w:word_count)
+        cout<<w.first<<" occurs "<<w.second<<((w.second>1)?" times":" time")<<endl;
 }
 
 void wordTransformation()
 {
-    std::ifstream ifs_map("../data/word_transformation.txt"),
-        ifs_content("../data/given_to_transform.txt");
-    if (!ifs_map || !ifs_content) {
-        std::cerr << "can't find the documents." << std::endl;
+    ifstream ifs_map("E:\\1.txt"), ifs_content("E:\\2.txt");
+    if(!ifs_map||!ifs_content)
+    {
+        cerr<<"can't find the documents"<<endl;
         return;
     }
 
-    std::unordered_map<string, string> trans_map;
-    for (string key, value; ifs_map >> key && getline(ifs_map, value);)
-        if (value.size() > 1)
-            trans_map[key] =
-                value.substr(1).substr(0, value.find_last_not_of(' '));
+    unordered_map<string, string> trans_map;
+    for(string key, value; ifs_map>>key&&getline(ifs_map, value); )
+        if(value.size()>1)
+            trans_map[key]=value.substr(1).substr(0, value.find_last_not_of(' '));
 
-    for (string text, word; getline(ifs_content, text); std::cout << std::endl)
-        for (std::istringstream iss(text); iss >> word;) {
-            auto map_it = trans_map.find(word);
-            std::cout << (map_it == trans_map.cend() ? word : map_it->second)
-                      << " ";
+    for(string text, word; getline(ifs_content, text); cout<<endl)
+        for(istringstream iss(text); iss>>word; )
+        {
+            auto map_it=trans_map.find(word);
+            cout<<((map_it==trans_map.cend()) ? word : map_it->second)<<" ";
         }
 }
 
 int main()
 {
-    // wordCounting();
+    //wordCounting();
     wordTransformation();
 }
