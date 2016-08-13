aa3d7a75770352ccac6677f0d61b2fa933950e33
zzz <zhangzhizhongz3@foxmail.com>
2016-08-13 15:32:08 +0800

    Update ex11_31.cpp

--- ch11/ex11_31.cpp
+++ ch11/ex11_31.cpp
@@ -10,32 +10,33 @@
 //  Be sure your program works correctly if the element you look for is not in
 //  the map.
 
+#include <iostream>
 #include <map>
 #include <string>
-#include <iostream>
 
-using std::string;
+using namespace std;
 
 int main()
 {
-    std::multimap<string, string> authors{
-        {"alan", "DMA"}, {"pezy", "LeetCode"}, {"alan", "CLRS"},
-        {"wang", "FTP"}, {"pezy", "CP5"},      {"wang", "CPP-Concurrency"}};
-    // want to delete an element that author is [Alan], work is [112].
-    string author = "pezy";
-    string work = "CP5";
+    multimap<string, string> authors={{"alan", "DMA"}, {"pezy", "LeetCode"}, {"alan", "CLRS"}, {"wang", "FTP"}, {"pezy", "CP5"}, {"wang", "CPP-Concurrency"}};
+    // want to delete an element that author is Alan, work is CP5
+    string author="pezy";
+    string work="CP5";
 
-    auto found = authors.find(author);
-    auto count = authors.count(author);
-    while (count) {
-        if (found->second == work) {
+    auto found=authors.find(author);
+    auto cnt=authors.count(author);
+
+    while(cnt)
+    {
+        if(found->second==work)
+        {
             authors.erase(found);
             break;
         }
         ++found;
-        --count;
+        --cnt;
     }
 
-    for (const auto& author : authors)
-        std::cout << author.first << " " << author.second << std::endl;
+    for(const auto &author:authors)
+        cout<<author.first<<" "<<author.second<<endl;
 }
