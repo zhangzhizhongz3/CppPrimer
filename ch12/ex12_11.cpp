c13e61a43873dbed267fa61c46297fd7d9b08f66
zzz <zhangzhizhongz3@foxmail.com>
2016-08-15 20:16:57 +0800

    Update ex12_11.cpp

--- ch12/ex12_11.cpp
+++ ch12/ex12_11.cpp
@@ -1,39 +1,27 @@
-/***************************************************************************
- *  @file       The code is for the exercises in C++ Primmer 5th Edition
- *  @author     Alan.W
- *  @date       23  DEC 2013
- *  @remark
- ***************************************************************************/
 //!
 //! Exercise 12.11:
 //! What would happen if we called process as follows?
-//  An error was generated at run time : double free or corruption.
-//  See the comments below.
+//! An error was generated at run time : double free or corruption.
+//!
 
 #include <iostream>
-#include <vector>
-#include <string>
 #include <memory>
 
-void process(std::shared_ptr<int> ptr)
+using namespace std;
+
+void process(shared_ptr<int> ptr)
 {
-    std::cout << "inside the process function:" << ptr.use_count() << "\n";
+    cout<<"inside the process function: "<<ptr.use_count()<<endl;
 }
 
 int main()
 {
-    std::shared_ptr<int> p(new int(42));
+    shared_ptr<int> p(new int(42));
 
-    /**
-     * @brief   std::shared_ptr<int>(p.get()) construct a temporary shared_ptr
-     * and copy it
-     *          to the parameter.However it is not a copy of p. As a result, at
-     * end of this
-     *          main function p will free the memory that has been freed inside
-     * process ().
-     *          That's why "double freed or corruption" was generated.
-     */
-    process(std::shared_ptr<int>(p.get()));
+    // @brief shared_ptr<int>(p.get() construct a temporary shared_ptr and copy it
+    //        to the parameter. However it is not a copy of p. As a result, at the
+    //        end of this main function p will free the memory that has been freed
+    //        inside process(). That's why "double freed or corruption" was generated.
 
-    return 0;
+    process(shared_ptr<int>(p.get()));
 }
