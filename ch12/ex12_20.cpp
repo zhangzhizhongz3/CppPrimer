1308e21373d92dfdf3f2b681d958094e91dac556
zzz <zhangzhizhongz3@foxmail.com>
2016-08-16 21:29:27 +0800

    Update ex12_20.cpp

--- ch12/ex12_20.cpp
+++ ch12/ex12_20.cpp
@@ -8,16 +8,19 @@
 //  Write a program that reads an input file a line at a time into
 //  a StrBlob and uses a StrBlobPtr to print each element in that StrBlob.
 
-#include "ex12_19.h"
-#include <fstream>
 #include <iostream>
+#include <fstream>
+#include "ex12_19.h"
+
+using namespace std;
 
 int main()
 {
-    std::ifstream ifs("../data/book.txt");
-    StrBlob blob;
-    for (std::string str; std::getline(ifs, str);) blob.push_back(str);
-    for (StrBlobPtr pbeg(blob.begin()), pend(blob.end()); pbeg != pend;
-         pbeg.incr())
-        std::cout << pbeg.deref() << std::endl;
+    ifstream ifs("E:\\zzz.txt");
+    StrBlob sb;
+    string s;
+    while(getline(ifs, s))
+        sb.push_back(s);
+    for(StrBlobPtr sbp=sb.begin(); sbp!=sb.end(); sbp.incr())
+        cout<<sbp.deref()<<endl;
 }
