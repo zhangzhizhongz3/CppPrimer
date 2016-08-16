150723edb6895d6f8172248df2c7cefb5cfc67de
zzz <zhangzhizhongz3@foxmail.com>
2016-08-16 22:16:18 +0800

    Update ex12_24.cpp

--- ch12/ex12_24.cpp
+++ ch12/ex12_24.cpp
@@ -13,18 +13,20 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    // need to tell the size.
-    std::cout << "How long do you want the string? ";
+    //need to tell the size
+    cout<<"How long do you want the string? ";
     int size{0};
-    std::cin >> size;
-    char* input = new char[size + 1]();
-    std::cin.ignore();
-    std::cout << "input the string: ";
-    std::cin.get(input, size + 1);
-    std::cout << input;
-    delete[] input;
-    // Test: if longer than the array size, we will lost the characters which
-    // are out of range.
+    cin>>size;
+    char *input=new char[size+1]();
+    cin.ignore();
+    cout<<"input the string: ";
+    cin.get(input, size+1);
+    cout<<input ;
+    delete [] input;
+    //Test: if longer than the array size, we will lost
+    //      the characters which are out of range
 }
