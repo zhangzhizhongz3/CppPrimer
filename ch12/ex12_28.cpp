957cd8bbfbb9ab35fef1df1ef715ab6e58a0bc34
zzz <zhangzhizhongz3@foxmail.com>
2016-08-17 22:33:37 +0800

    Update ex12_28.cpp

--- ch12/ex12_28.cpp
+++ ch12/ex12_28.cpp
@@ -13,53 +13,53 @@
 //  to generate the results for the query.
 //
 
-#include <string>
-using std::string;
-
-#include <vector>
-using std::vector;
-
-#include <memory>
-using std::shared_ptr;
-
 #include <iostream>
+#include <vector>
+#include <string>
 #include <fstream>
 #include <sstream>
 #include <map>
 #include <set>
 #include <algorithm>
+#include <iterator>
+#include <cctype>
+
+using namespace std;
 
 int main()
 {
-    std::ifstream file("../data/letter.txt");
-    vector<string> input;
-    std::map<string, std::set<decltype(input.size())>> dictionary;
-    decltype(input.size()) lineNo{0};
-
-    for (string line; std::getline(file, line); ++lineNo) {
-        input.push_back(line);
-        std::istringstream line_stream(line);
-        for (string text, word; line_stream >> text; word.clear()) {
-            std::remove_copy_if(text.begin(), text.end(),
-                                std::back_inserter(word), ispunct);
-            dictionary[word].insert(lineNo);
+    ifstream infile("E:\\zzz.txt");
+    vector<string> file;
+    map<string, set<decltype(file.size())>> wm;
+    string line;
+    while(getline(infile, line))
+    {
+        file.push_back(line);
+        int n=file.size()-1;
+        istringstream in(line);
+        string text, word;
+        while(in>>text)
+        {
+            remove_copy_if(text.begin(), text.end(), back_inserter(word), [](const char &ch) {return ispunct(ch);});
+            wm[word].insert(n);
+            word.clear();
         }
     }
 
-    while (true) {
-        std::cout << "enter word to look for, or q to quit: ";
+    while(true)
+    {
+        cout<<"enter word to look for, or q to quit: ";
         string s;
-        if (!(std::cin >> s) || s == "q") break;
-        auto found = dictionary.find(s);
-        if (found != dictionary.end()) {
-            std::cout << s << " occurs " << found->second.size()
-                      << (found->second.size() > 1 ? " times" : " time")
-                      << std::endl;
-            for (auto i : found->second)
-                std::cout << "\t(line " << i + 1 << ") " << input.at(i)
-                          << std::endl;
+        if(!(cin>>s)||s=="q")
+            break;
+        auto loc=wm.find(s);
+        if(loc!=wm.end())
+        {
+            cout<<s<<" ocurrs "<<loc->second.size()<<(loc->second.size()>1?" times":" time")<<endl;
+            for(auto num:loc->second)
+                cout<<"\t(line "<<num+1<<")"<<file.at(num)<<endl;
         }
         else
-            std::cout << s << " occurs 0 time" << std::endl;
+            cout<<s<<" ocurrs 0 time"<<endl;
     }
 }
