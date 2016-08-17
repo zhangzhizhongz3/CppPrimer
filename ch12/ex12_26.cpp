748bf12ad8eaa0ece6850092528ef4d9116ca78f
zzz <zhangzhizhongz3@foxmail.com>
2016-08-17 10:06:23 +0800

    Update ex12_26.cpp

--- ch12/ex12_26.cpp
+++ ch12/ex12_26.cpp
@@ -6,21 +6,26 @@
 //  Copyright (c) 2014 pezy. All rights reserved.
 //
 //  Rewrite the program on page 481 using an allocator.
+//
 
 #include <iostream>
-#include <string>
+#include <iostream>
 #include <memory>
+#include <string>
+
+using namespace std;
 
 void input_reverse_output_string(int n)
 {
-    std::allocator<std::string> alloc;
-    auto const p = alloc.allocate(n);
-    std::string s;
-    auto q = p;
-    while (std::cin >> s && q != p + n) alloc.construct(q++, s);
-
-    while (q != p) {
-        std::cout << *--q << " ";
+    allocator<string> alloc;
+    auto const p=alloc.allocate(n);
+    string s;
+    auto q=p;
+    while(cin>>s&&q!=p+n)
+        alloc.construct(q++, s);
+    while(q!=p)
+    {
+        cout<<*--q<<" ";
         alloc.destroy(q);
     }
     alloc.deallocate(p, n);
