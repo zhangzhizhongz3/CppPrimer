f10de302ab376fe0281828b6fc0040adfa4b9d92
zzz <zhangzhizhongz3@foxmail.com>
2016-08-18 08:36:24 +0800

    Update ex12_32.h

--- ch12/ex12_32.h
+++ ch12/ex12_32.h
@@ -7,46 +7,45 @@
 //
 //  Rewrite the TextQuery and QueryResult classes to use a StrBlob
 //  instead of a vector<string> to hold the input file.
+//
 
 #ifndef CP5_ex12_32_h
 #define CP5_ex12_32_h
-
-#include "ex12_22.h"
-using std::shared_ptr;
 
 #include <iostream>
 #include <fstream>
 #include <map>
 #include <set>
+#include <memory>
+#include "ex12_22.h"
+
+using namespace std;
 
 class QueryResult;
+
 class TextQuery {
 public:
-    TextQuery(std::ifstream&);
+    TextQuery(ifstream&);
+
     QueryResult query(const string&) const;
 
 private:
-    shared_ptr<StrBlob> input;
-    std::map<string, shared_ptr<std::set<StrBlob::size_type>>> result;
+    shared_ptr<StrBlob> file;
+    map<string, shared_ptr<set<StrBlob::size_type>>> wm;
 };
 
 class QueryResult {
-public:
-    friend std::ostream& print(std::ostream&, const QueryResult&);
+friend ostream &print(ostream&, const QueryResult&);
 
 public:
-    QueryResult(const string& s, shared_ptr<std::set<StrBlob::size_type>> set,
-                shared_ptr<StrBlob> v)
-        : word(s), nos(set), input(v)
-    {
-    }
+    QueryResult(string s, shared_ptr<set<StrBlob::size_type>> p, shared_ptr<StrBlob> f):sought(s), lines(p), file(f) {}
 
 private:
-    string word;
-    shared_ptr<std::set<StrBlob::size_type>> nos;
-    shared_ptr<StrBlob> input;
+    string sought;
+    shared_ptr<set<StrBlob::size_type>> lines;
+    shared_ptr<StrBlob> file;
 };
 
-std::ostream& print(std::ostream&, const QueryResult&);
+ostream &print(ostream&, const QueryResult&);
 
 #endif
