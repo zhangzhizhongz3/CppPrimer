f5ae38828738e0a00f5c18b9ecf5e80718109389
zzz <zhangzhizhongz3@foxmail.com>
2016-08-18 19:39:07 +0800

    Update ex13_05.h

--- ch13/ex13_05.h
+++ ch13/ex13_05.h
@@ -1,6 +1,5 @@
 //
 //  ex13_05.h
-//  CP5
 //
 //  Created by pezy on 1/5/15.
 //  Copyright (c) 2015 pezy. All rights reserved.
@@ -8,23 +7,14 @@
 //  Given the following sketch of a class, write a copy constructor that copies
 //  all the members.
 //  Your constructor should dynamically allocate a new string and copy the
-//  object to which ps points,
-//  rather than copying ps itself.
-
-#ifndef CP5_ex13_05_h
-#define CP5_ex13_05_h
-
-#include <string>
+//  object to which ps points, rather than copying ps itself.
+//
 
 class HasPtr {
 public:
-    HasPtr(const std::string& s = std::string()) : ps(new std::string(s)), i(0)
-    {
-    }
-    HasPtr(const HasPtr& hp) : ps(new std::string(*hp.ps)), i(hp.i) {}
+    HasPtr(const string &s = string()) : ps(new string(s)), i(0) {}
+    HasPtr(const HasPtr &hp) : ps(new string(*hp.ps)), i(hp.i) {}
 private:
-    std::string* ps;
+    string *ps;
     int i;
 };
-
-#endif
