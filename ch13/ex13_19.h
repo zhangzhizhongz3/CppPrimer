d5317b043233dabf7427f70c0effb0338e1af8af
zzz <zhangzhizhongz3@foxmail.com>
2016-08-19 21:44:30 +0800

    Update ex13_19.h

--- ch13/ex13_19.h
+++ ch13/ex13_19.h
@@ -5,10 +5,9 @@
 //  Created by pezy on 1/15/15.
 //  Copyright (c) 2015 pezy. All rights reserved.
 //
-//  Does your Employee class need to define its own versions of the copy-control
-//  members?
-//  If so, why? If not, why not?
-//  Implement whatever copy-control members you think Employee needs.
+//  Does your Employee class need to define its own versions of the copy control
+//  members? If so, why? If not, why not? Implement whatever copy control members 
+//  you think Employee needs.
 //
 //  Answer: No, cause there really is no sensible meaning. employee can't copy
 //  in real world.
@@ -17,21 +16,20 @@
 #define CP5_ex13_19_h
 
 #include <string>
-using std::string;
+using namespace std;
 
 class Employee {
 public:
     Employee();
-    Employee(const string& name);
-    Employee(const Employee&) = delete;
-    Employee& operator=(const Employee&) = delete;
-
-    const int id() const { return id_; }
-
+    Employee(const string &name);
+    Employee(const Employee&)=delete;
+    Employee &operator=(const Employee&)=delete;
+    
+    const int id() const {return id_;}
 private:
     string name_;
     int id_;
-    static int s_increment;
+    statc int s_increment;
 };
 
 #endif
