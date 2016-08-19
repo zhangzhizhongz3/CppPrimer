1f5d635a865ac33f18ff5724f398161545c8785f
zzz <zhangzhizhongz3@foxmail.com>
2016-08-19 10:24:59 +0800

    Update ex13_13.cpp

--- ch13/ex13_13.cpp
+++ ch13/ex13_13.cpp
@@ -30,7 +30,7 @@ struct X {
     X(const X&) {cout<<"X(const X&)"<<endl;}
     X &operator=(const X&)
     {
-        cout<<"X &operator(const X&)"<<endl;
+        cout<<"X &operator=(const X&)"<<endl;
         return *this;
     }
     ~X() {cout<<"~X()"<<endl;}
