a9bff45ee71ada0d79a2592c07b05d84f3c30d5f
zzz <zhangzhizhongz3@foxmail.com>
2016-08-19 22:48:01 +0800

    Update ex13_22.h

--- ch13/ex13_22.h
+++ ch13/ex13_22.h
@@ -5,39 +5,38 @@
 //  Created by pezy on 1/13/15.
 //  Copyright (c) 2015 pezy. All rights reserved.
 //
-//  Assume that we want HasPtr to behave like a value.
-//  That is, each object should have its own copy of the string to which the
-//  objects point.
-//  We¡¯ll show the definitions of the copy-control members in the next section.
+//  Assume that we want HasPtr to behave like a value. That is, each object 
+//  should have its own copy of the string to which the objects point.
+//  We'll show the definitions of the copy-control members in the next section.
 //  However, you already know everything you need to know to implement these
 //  members.
-//  Write the HasPtr copy constructor and copyassignment operator before reading
+//  Write the HasPtr copy constructor and copy-assignment operator before reading
 //  on.
 //
 //  See ex13_11.h
+//
 
-#ifndef CP5_ex13_11_h
-#define CP5_ex13_11_h
+#ifndef CP5_ex13_22_h
+#define CP5_ex13_22_h
 
 #include <string>
 
+using namespace std;
+
 class HasPtr {
 public:
-    HasPtr(const std::string& s = std::string()) : ps(new std::string(s)), i(0)
+    HasPtr(const string &s = string()) : ps(new string(s)), i(0) {}
+    HasPtr(const HasPtr &hp) : ps(new string(*hp.ps)), i(hp.i) {}
+    HasPtr &operator=(const HasPtr &hp)
     {
-    }
-    HasPtr(const HasPtr& hp) : ps(new std::string(*hp.ps)), i(hp.i) {}
-    HasPtr& operator=(const HasPtr& hp)
-    {
-        auto new_p = new std::string(*hp.ps);
         delete ps;
-        ps = new_p;
-        i = hp.i;
+        ps=new string(*hp.ps);
+        i=hp.i;
         return *this;
     }
-    ~HasPtr() { delete ps; }
+    ~HasPtr() {delete ps;}
 private:
-    std::string* ps;
+    string *ps;
     int i;
 };
 
