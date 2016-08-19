fae4f6abf9e5cf67d41ffe4c2bdf40b983d123fc
zzz <zhangzhizhongz3@foxmail.com>
2016-08-19 09:30:07 +0800

    Update ex13_11.h

--- ch13/ex13_11.h
+++ ch13/ex13_11.h
@@ -1,6 +1,5 @@
 //
 //  ex13_11.h
-//  CP5
 //
 //  Created by pezy on 1/13/15.
 //  Copyright (c) 2015 pezy. All rights reserved.
@@ -8,30 +7,21 @@
 //  Add a destructor to your HasPtr class from the previous exercises.
 //
 //  See ex13_08.h
-
-#ifndef CP5_ex13_11_h
-#define CP5_ex13_11_h
-
-#include <string>
+//
 
 class HasPtr {
 public:
-    HasPtr(const std::string& s = std::string()) : ps(new std::string(s)), i(0)
+    HasPtr(const string &s = string()) : ps(new string(s)), i(0) {}
+    HasPtr(const HasPtr &hp) : ps(new string(*hp.ps)), i(hp.i) {}
+    HasPtr &operator=(const HasPtr &hp)
     {
-    }
-    HasPtr(const HasPtr& hp) : ps(new std::string(*hp.ps)), i(hp.i) {}
-    HasPtr& operator=(const HasPtr& hp)
-    {
-        std::string* new_ps = new std::string(*hp.ps);
         delete ps;
-        ps = new_ps;
-        i = hp.i;
+        ps=new string(*hp.ps);
+        i=hp.i;
         return *this;
     }
-    ~HasPtr() { delete ps; }
+    ~HasPtr() {delete ps;}
 private:
-    std::string* ps;
+    string *ps;
     int i;
 };
-
-#endif
