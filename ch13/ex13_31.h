f2076575a794b240b49101bdc3f0144957046633
zzz <zhangzhizhongz3@foxmail.com>
2016-08-20 18:02:31 +0800

    Update ex13_31.h

--- ch13/ex13_31.h
+++ ch13/ex13_31.h
@@ -10,50 +10,51 @@
 //  Note when swap is called.
 //
 //  See ex13_30.h
+//
 
-#ifndef CP5_ex13_11_h
-#define CP5_ex13_11_h
+#ifndef CP5_ex13_31_h
+#define CP5_ex13_31_h
 
-#include <string>
 #include <iostream>
+#include <string>
+using namespace std;
 
 class HasPtr {
+friend void swap(HasPtr&, HasPtr&);
+friend bool operator<(const HasPtr&, const HasPtr&);
 public:
-    friend void swap(HasPtr&, HasPtr&);
-    friend bool operator<(const HasPtr& lhs, const HasPtr& rhs);
-    HasPtr(const std::string& s = std::string()) : ps(new std::string(s)), i(0)
-    {
-    }
-    HasPtr(const HasPtr& hp) : ps(new std::string(*hp.ps)), i(hp.i) {}
-    HasPtr& operator=(HasPtr tmp)
+    HasPtr(const string &s=string()):ps(new string(s)), i(0) {}
+    HasPtr(const HasPtr &p):ps(new string(*p.ps)), i(p.i) {}
+    HasPtr &operator=(HasPtr tmp)
     {
         this->swap(tmp);
         return *this;
     }
-    ~HasPtr() { delete ps; }
+    ~HasPtr() {delete ps;}
 
-    void swap(HasPtr& rhs)
+    void swap(HasPtr &rhs)
     {
-        using std::swap;
-        swap(ps, rhs.ps);
-        swap(i, rhs.i);
-        std::cout << "call swap(HasPtr &rhs)" << std::endl;
+       using std::swap;
+       swap(ps,rhs.ps);
+       swap(i, rhs.i);
+       cout<<"call swap(HasPtr&)"<<endl;
     }
 
-    void show() const { std::cout << *ps << std::endl; }
+    void show() const {cout<<*ps<<endl;}
+
 private:
-    std::string* ps;
+    string *ps;
     int i;
 };
 
-void swap(HasPtr& lhs, HasPtr& rhs)
+void swap(HasPtr &lhs, HasPtr &rhs)
 {
     lhs.swap(rhs);
 }
 
-bool operator<(const HasPtr& lhs, const HasPtr& rhs)
+bool operator<(const HasPtr &lhs, const HasPtr &rhs)
 {
-    return *lhs.ps < *rhs.ps;
+    return *lhs.ps<*rhs.ps;
 }
 
 #endif
