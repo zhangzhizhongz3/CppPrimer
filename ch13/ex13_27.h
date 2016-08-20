40446a2c6670163485fc13905b895607fa8e04d2
zzz <zhangzhizhongz3@foxmail.com>
2016-08-20 14:31:12 +0800

    Update ex13_27.h

--- ch13/ex13_27.h
+++ ch13/ex13_27.h
@@ -6,43 +6,44 @@
 //  Copyright (c) 2015 pezy. All rights reserved.
 //
 //  Define your own reference-counted version of HasPtr.
+//
 
 #ifndef CP5_ex13_27_h
 #define CP5_ex13_27_h
 
 #include <string>
+using namespace std;
 
 class HasPtr {
 public:
-    HasPtr(const std::string& s = std::string())
-        : ps(new std::string(s)), i(0), use(new size_t(1))
-    {
-    }
-    HasPtr(const HasPtr& hp) : ps(hp.ps), i(hp.i), use(hp.use) { ++*use; }
-    HasPtr& operator=(const HasPtr& rhs)
+    HasPtr(const string &s=string()):ps(new string(s)), i(0), use(new size_t(1)) {}
+    HasPtr(const HasPtr &p):ps(p.ps), i(p.i), use(p.use) {++*use;}
+    HasPtr &operator=(const HasPtr &rhs)
     {
         ++*rhs.use;
-        if (--*use == 0) {
+        if(--*use==0)
+        {
             delete ps;
             delete use;
         }
-        ps = rhs.ps;
-        i = rhs.i;
-        use = rhs.use;
+        ps=rhs.ps;
+        i=rhs.i;
+        use=rhs.use;
         return *this;
     }
     ~HasPtr()
     {
-        if (--*use == 0) {
+        if(--*use==0)
+        {
             delete ps;
             delete use;
         }
     }
-
+    
 private:
-    std::string* ps;
+    string *ps;
     int i;
-    size_t* use;
+    size_t *use;
 };
 
 #endif
