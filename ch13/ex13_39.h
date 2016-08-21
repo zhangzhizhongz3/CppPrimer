1c590476f98a6f087133ccc1de242d1161ad5079
zzz <zhangzhizhongz3@foxmail.com>
2016-08-21 13:44:09 +0800

    Update ex13_39.h

--- ch13/ex13_39.h
+++ ch13/ex13_39.h
@@ -9,45 +9,44 @@
 //  reserve, capacity (9.4, p. 356), and resize (9.3.5, p. 352).
 //
 
-#ifndef CP5_EX_13_39_H_
-#define CP5_EX_13_39_H_
+#ifndef CP5_ex13_39_h
+#define CP5_ex13_39_h
 
-#include <memory>
 #include <string>
+#include <memory>
+#include <utility>
+using namespace std;
 
 class StrVec {
 public:
-    StrVec() : elements(nullptr), first_free(nullptr), cap(nullptr) {}
+    StrVec():elements(nullptr), first_free(nullptr), cap(nullptr) {}
     StrVec(const StrVec&);
-    StrVec& operator=(const StrVec&);
+    StrVec &operator=(const StrVec&);
     ~StrVec();
 
-    void push_back(const std::string&);
-    size_t size() const { return first_free - elements; }
-    size_t capacity() const { return cap - elements; }
-    std::string* begin() const { return elements; }
-    std::string* end() const { return first_free; }
+    void push_back(const string&);
+    size_t size() const (return first_free-elements;)
+    size_t capacity() const {return cap-elements;}
+    string *begin() const {return elements;}
+    string *end() const {return first_free;}
 
-    void reserve(size_t new_cap);
-    void resize(size_t count);
-    void resize(size_t count, const std::string&);
+    void reserve(size_t);
+    void resize(size_t);
+    void resize(size_t, const string&);
 
 private:
-    std::pair<std::string*, std::string*> alloc_n_copy(const std::string*,
-                                                       const std::string*);
+    static allocator<string> alloc;
+
+    void chk_n_alloc() {if(size()==capacity()) reallocate();}
+
+    pair<string*, string*> alloc_n_copy(const string*, const string*);
     void free();
-    void chk_n_alloc()
-    {
-        if (size() == capacity()) reallocate();
-    }
     void reallocate();
-    void alloc_n_move(size_t new_cap);
+    void alloc_n_move(size_t);
 
-private:
-    std::string* elements;
-    std::string* first_free;
-    std::string* cap;
-    std::allocator<std::string> alloc;
+    string *elements;
+    string *first_free;
+    string *cap;
 };
 
 #endif
