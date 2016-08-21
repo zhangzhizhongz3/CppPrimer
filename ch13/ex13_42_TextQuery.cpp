4e57f16b8beb67022c7ed310ba7be57955fa0fc2
zzz <zhangzhizhongz3@foxmail.com>
2016-08-21 18:18:17 +0800

    Update ex13_42_TextQuery.cpp

--- ch13/ex13_42_TextQuery.cpp
+++ ch13/ex13_42_TextQuery.cpp
@@ -1,43 +1,54 @@
-#include "ex13_42_TextQuery.h"
+#include <iostream>
+#include <fstream>
 #include <sstream>
+#include <string>
+#include <cstddef>
 #include <algorithm>
+#include <iterator>
+#include <cctype>
+#include "ex13_40.h"
+#include "ex12_27_30.h"
 
-using std::string;
+using namespace std;
 
-TextQuery::TextQuery(std::ifstream& ifs) : input(new StrVec)
+TextQuery::TextQuery(ifstream &is):file(new StrVec)
 {
-    size_t lineNo = 0;
-    for (string line; std::getline(ifs, line); ++lineNo) {
-        input->push_back(line);
-        std::istringstream line_stream(line);
-        for (string text, word; line_stream >> text; word.clear()) {
-            // avoid read a word followed by punctuation(such as: word, )
-            std::remove_copy_if(text.begin(), text.end(),
-                                std::back_inserter(word), ispunct);
-            // use reference avoid count of shared_ptr add.
-            auto& nos = result[word];
-            if (!nos) nos.reset(new std::set<size_t>);
-            nos->insert(lineNo);
+    string line;
+    while(getline(is, line))
+    {
+        file->push_back(line);
+        int n=file->size()-1;
+        istringstream in(line);
+        string text, word;
+        while(in>>text)
+        {
+            //avoid read a word followed by punctuation(such as: word,)
+            remove_copy_if(text.begin(), text.end(), back_inserter(word), [](const char &ch) {return ispunct(ch);});
+            auto &lines=wm[word];
+            if(!lines)
+                lines.reset(new set<size_t>);
+            lines->insert(n);
+            word.clear();
         }
     }
 }
 
-QueryResult TextQuery::query(const string& str) const
+QueryResult TextQuery::query(const string &sought) const
 {
-    // use static just allocate once.
-    static std::shared_ptr<std::set<size_t>> nodate(new std::set<size_t>);
-    auto found = result.find(str);
-    if (found == result.end())
-        return QueryResult(str, nodate, input);
+    //use static just allocate once
+    static shared_ptr<set<size_t>> nodata(new set<size_t>);
+
+    auto loc=wm.find(sought);
+    if(loc==wm.end())
+        return QueryResult(sought, nodata, file);
     else
-        return QueryResult(str, found->second, input);
+        return QueryResult(sought, loc->second, file);
 }
 
-std::ostream& print(std::ostream& out, const QueryResult& qr)
+ostream &print(ostream &os, const QueryResult &qr)
 {
-    out << qr.word << " occurs " << qr.nos->size()
-        << (qr.nos->size() > 1 ? " times" : " time") << std::endl;
-    for (auto i : *qr.nos)
-        out << "\t(line " << i + 1 << ") " << qr.input->at(i) << std::endl;
-    return out;
+    os<<qr.sought<<" occurs "<<qr.lines->size()<<(qr.lines->size()>1 ? " times" : " time")<<endl;
+    for(auto num : *qr.lines)
+        os<<"\t(line "<<num+1<<") "<<qr.file->at(num)<<endl;
+    return os;
 }
