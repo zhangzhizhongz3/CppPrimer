5ad7d3511e8497c2fc0d4b994cfa337b4a154060
zzz <zhangzhizhongz3@foxmail.com>
2016-08-21 08:45:59 +0800

    Update ex13_34_36_37.h

--- ch13/ex13_34_36_37.h
+++ ch13/ex13_34_36_37.h
@@ -11,68 +11,71 @@
 //   hold a set that points to the Messages in that Folder.
 //
 //   37:  Add members to the Message class to insert or remove a given Folder*
-//   into folders.
-//        These members are analogous to Folder’s addMsg and remMsg operations.
+//   into folders. These members are analogous to Folder’s addMsg and remMsg operations.
+//
 
 #ifndef CP5_ex13_34_36_37_h
 #define CP5_ex13_34_36_37_h
 
-#include <set>
 #include <string>
+#include <set>
+using namespace std;
 
 class Folder;
 
 class Message {
-    friend void swap(Message&, Message&);
-    friend void swap(Folder&, Folder&);
-    friend class Folder;
+friend class Folder;
+friend void swap(Message&, Message&);
+friend void swap(Folder&, Folder&);
 
 public:
-    explicit Message(const std::string& str = "") : contents(str) {}
+    explicit Message(const string &str=""):contents(str) {}
     Message(const Message&);
-    Message& operator=(const Message&);
+    Message &operator=(const Message&);
     ~Message();
+
     void save(Folder&);
     void remove(Folder&);
 
     void print_debug();
 
 private:
-    std::string contents;
-    std::set<Folder*> folders;
+    string contents;
+    set<Folder*> folders;
+
 
     void add_to_Folders(const Message&);
     void remove_from_Folders();
 
-    void addFldr(Folder* f) { folders.insert(f); }
-    void remFldr(Folder* f) { folders.erase(f); }
+    void addFldr(Folder *f) {folders.insert(f);}
+    void remFldr(Folder *f) {folders.erase(f);}
 };
 
 void swap(Message&, Message&);
 
 class Folder {
-    friend void swap(Message&, Message&);
-    friend void swap(Folder&, Folder&);
-    friend class Message;
+friend class Message;
+friend void swap(Message&, Message&);
+friend void swap(Folder&, Folder&);
 
 public:
-    Folder() = default;
+    Folder()=default;
     Folder(const Folder&);
-    Folder& operator=(const Folder&);
+    Folder &operator=(const Folder&);
     ~Folder();
 
     void print_debug();
 
 private:
-    std::set<Message*> msgs;
+    set<Message*> msgs;
 
     void add_to_Messages(const Folder&);
     void remove_from_Messages();
 
-    void addMsg(Message* m) { msgs.insert(m); }
-    void remMsg(Message* m) { msgs.erase(m); }
+    void addMsg(Message *m) {msgs.insert(m);}
+    void remMsg(Message *m) {msgs.erase(m);}
 };
 
 void swap(Folder&, Folder&);
 
-#endif // MESSAGE
+#endif
