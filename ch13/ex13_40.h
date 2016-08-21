24ff279a6e018bb1a4feb6a96a6f9cf1153c9b94
zzz <zhangzhizhongz3@foxmail.com>
2016-08-21 18:14:26 +0800

    Update ex13_40.h

--- ch13/ex13_40.h
+++ ch13/ex13_40.h
@@ -36,8 +36,6 @@ public:
     void resize(size_t, const string&);
 
 private:
-    static allocator<string> alloc;
-
     void chk_n_alloc() {if(size()==capacity()) reallocate();}
 
     pair<string*, string*> alloc_n_copy(const string*, const string*);
@@ -49,6 +47,7 @@ private:
     string *elements;
     string *first_free;
     string *cap;
+    allocator<string> alloc;
 };
 
 #endif
