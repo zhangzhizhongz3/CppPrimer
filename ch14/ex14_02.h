2794815dcd91e6bdc5c28172adeabdce55371a63
zzz <zhangzhizhongz3@foxmail.com>
2016-08-23 19:13:25 +0800

    Update ex14_02.h

--- ch14/ex14_02.h
+++ ch14/ex14_02.h
@@ -45,7 +45,6 @@ double Sales_data::avg_price() const
     return units_sold ? revenue / units_sold : 0;
 }
 
-// declarations for nonmember parts of the Sales_data interface
 std::istream& operator>>(std::istream&, Sales_data&);
 std::ostream& operator<<(std::ostream&, const Sales_data&);
 Sales_data operator+(const Sales_data&, const Sales_data&);
