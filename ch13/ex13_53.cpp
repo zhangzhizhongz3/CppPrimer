eea4c6ebf1e848e57dadf8fb241d066ec699101a
zzz <zhangzhizhongz3@foxmail.com>
2016-08-23 10:36:39 +0800

    Update ex13_53.cpp

--- ch13/ex13_53.cpp
+++ ch13/ex13_53.cpp
@@ -1,61 +1,67 @@
-#include "ex13_53.h"
+#include <string>
 #include <iostream>
+#include "ex13_53.h"
+using namespace std;
 
-inline void swap(HasPtr& lhs, HasPtr& rhs)
+void swap(HasPtr &lhs, HasPtr &rhs)
 {
     using std::swap;
     swap(lhs.ps, rhs.ps);
     swap(lhs.i, rhs.i);
-    std::cout << "call swap" << std::endl;
+    cout<<"call swap"<<endl;
 }
 
-HasPtr::HasPtr(const std::string& s) : ps(new std::string(s)), i(0)
+HasPtr::HasPtr(const string &s=string()):ps(new string(s)), i(0)
 {
-    std::cout << "call constructor" << std::endl;
+    cout<<"call constructor"<<endl;
 }
 
-HasPtr::HasPtr(const HasPtr& hp) : ps(new std::string(*hp.ps)), i(hp.i)
+HasPtr::HasPtr(const HasPtr &p):ps(new string(*p.ps)), i(p.i)
 {
-    std::cout << "call copy constructor" << std::endl;
+    cout<<"call copy constructor"<<endl;
 }
 
-HasPtr::HasPtr(HasPtr&& p) noexcept : ps(p.ps), i(p.i)
+HasPtr::HasPtr(HasPtr &&p) noexcept : ps(p.ps), i(p.i)
 {
-    p.ps = 0;
-    std::cout << "call move constructor" << std::endl;
+    p.ps=0;
+    cout<<"call move constructor"<<endl;
 }
 
-HasPtr& HasPtr::operator=(HasPtr rhs)
+HasPtr &HasPtr::operator=(HasPtr rhs)
 {
     swap(*this, rhs);
     return *this;
 }
 
-// HasPtr& HasPtr::operator=(const HasPtr &rhs)
-//{
-//    auto newp = new std::string(*rhs.ps);
-//    delete ps;
-//    ps = newp;
-//    i = rhs.i;
-//    std::cout << "call copy assignment" << std::endl;
-//    return *this;
-//}
+/*
+HasPtr &HasPtr::operator=(const HasPtr &rhs)
+{
+    auto newp=new string(*rhs.ps);
+    delete ps;
+    ps=newp;
+    i=rhs.i;
+    cout<<"call copy-assignment operator"<<endl;
+    return *this;
+}
+*/
 
-// HasPtr& HasPtr::operator=(HasPtr &&rhs) noexcept
-//{
-//    if (this != &rhs)
-//    {
-//        delete ps;
-//        ps = rhs.ps;
-//        i = rhs.i;
-//        rhs.ps = nullptr;
-//        std::cout << "call move assignment" << std::endl;
-//    }
-//    return *this;
-//}
+/*
+HasPtr &HasPtr::operator=(HasPtr &&rhs) noexcept
+{
+    if(this!=&rhs)
+    {
+        delete ps;
+        ps=rhs.ps;
+        i=rhs.i;
+        rhs.ps=nullptr;
+        cout<<"call move-assignment operator"<<endl;
+    }
+    return *this;
+}
+*/
 
 HasPtr::~HasPtr()
 {
-    std::cout << "call destructor" << std::endl;
     delete ps;
+    cout<<"call destructor"<<endl;
 }
