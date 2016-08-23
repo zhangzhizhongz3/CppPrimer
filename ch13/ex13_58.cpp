fa5ad7968f63ed979dda219026c680aa55d62369
zzz <zhangzhizhongz3@foxmail.com>
2016-08-23 09:29:29 +0800

    Update ex13_58.cpp

--- ch13/ex13_58.cpp
+++ ch13/ex13_58.cpp
@@ -1,15 +1,13 @@
-#include <vector>
 #include <iostream>
+#include <vector>
 #include <algorithm>
-
-using std::vector;
-using std::sort;
+using namespace std;
 
 class Foo {
 public:
-    Foo sorted()&&;
-    Foo sorted() const&;
-
+    Foo sorted() &&;
+    Foo sorted () const &;
+    
 private:
     vector<int> data;
 };
@@ -17,28 +15,29 @@ private:
 Foo Foo::sorted() &&
 {
     sort(data.begin(), data.end());
-    std::cout << "&&" << std::endl; // debug
+    cout<<"&&"<<endl;   //debug
     return *this;
 }
 
 Foo Foo::sorted() const &
 {
-    //    Foo ret(*this);
-    //    sort(ret.data.begin(), ret.data.end());
-    //    return ret;
-
-    std::cout << "const &" << std::endl; // debug
-
-    //    Foo ret(*this);
-    //    ret.sorted();     // Exercise 13.56
-    //    return ret;
-
-    return Foo(*this).sorted(); // Exercise 13.57
+    //Foo ret(*this);
+    //sort(ret.data.begin(), ret.data.end());
+    //return ret;
+    
+    cout<<"const &"<<endl;   //debug
+    
+    //Exercise 13.56
+    //Foo ret(*this);
+    //return ret.sorted();
+    
+    //Exercise 13.57
+    return Foo(*this).sorted();
 }
 
 int main()
 {
-    Foo().sorted(); // call "&&"
+    Foo().sorted(); //call "&&"
     Foo f;
-    f.sorted(); // call "const &"
+    f.sorted(); //call "const &"
 }
