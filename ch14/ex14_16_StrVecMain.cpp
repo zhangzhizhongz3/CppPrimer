4924f9e91c8e94bb5429adf423ca869b7ed305db
zzz <zhangzhizhongz3@foxmail.com>
2016-08-24 19:23:20 +0800

    Update ex14_16_StrVecMain.cpp

--- ch14/ex14_16_StrVecMain.cpp
+++ ch14/ex14_16_StrVecMain.cpp
@@ -1,15 +1,16 @@
-#include "ex14_16_StrVec.h"
 #include <iostream>
 #include <vector>
+#include "ex14_16_StrVec.h"
+using namespace std;
 
 int main()
 {
     StrVec vec;
     vec.reserve(6);
-    std::cout << "capacity(reserve to 6): " << vec.capacity() << std::endl;
+    cout << "capacity(reserve to 6): " << vec.capacity() << endl;
 
     vec.reserve(4);
-    std::cout << "capacity(reserve to 4): " << vec.capacity() << std::endl;
+    cout << "capacity(reserve to 4): " << vec.capacity() << endl;
 
     vec.push_back("hello");
     vec.push_back("world");
@@ -17,25 +18,25 @@ int main()
     vec.resize(4);
 
     for (auto i = vec.begin(); i != vec.end(); ++i)
-        std::cout << *i << std::endl;
-    std::cout << "-EOF-" << std::endl;
+        cout << *i << endl;
+    cout << "-EOF-" << endl;
 
     vec.resize(1);
 
     for (auto i = vec.begin(); i != vec.end(); ++i)
-        std::cout << *i << std::endl;
-    std::cout << "-EOF-" << std::endl;
+        cout << *i << endl;
+    cout << "-EOF-" << endl;
 
     StrVec vec_list{"hello", "world", "pezy"};
 
     for (auto i = vec_list.begin(); i != vec_list.end(); ++i)
-        std::cout << *i << " ";
-    std::cout << std::endl;
+        cout << *i << " ";
+    cout << endl;
 
     // Test operator==
-
     const StrVec const_vec_list{"hello", "world", "pezy"};
     if (vec_list == const_vec_list)
-        for (const auto& str : const_vec_list) std::cout << str << " ";
-    std::cout << std::endl;
+        for (const auto& str : const_vec_list)
+            cout << str << " ";
+    cout << endl;
 }
