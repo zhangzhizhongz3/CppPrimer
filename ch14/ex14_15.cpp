ebd48c7952fa9701e55ef532139289595aedafe8
zzz <zhangzhizhongz3@foxmail.com>
2016-08-24 10:00:34 +0800

    Update ex14_15.cpp

--- ch14/ex14_15.cpp
+++ ch14/ex14_15.cpp
@@ -1,50 +1,50 @@
+#include <iostream>
 #include "ex14_15.h"
 
-std::istream& operator>>(std::istream& in, Book& book)
+Book& Book::operator+=(const Book& rhs)
 {
-    in >> book.no_ >> book.name_ >> book.author_ >> book.pubdate_ >>
-        book.number_;
-    if (!in) book = Book();
-    return in;
+    if(rhs==*this)
+        number_+=rhs.number_;
+    return *this;
 }
 
-std::ostream& operator<<(std::ostream& out, const Book& book)
+std::istream& operator>>(std::istream& is, Book& book)
 {
-    out << book.no_ << " " << book.name_ << " " << book.author_ << " "
-        << book.pubdate_ << " " << book.number_ << std::endl;
-    return out;
+    is >> book.no_ >> book.name_ >> book.author_ >> book.pubdate_>>book.number_;
+    if(!is)
+        book=Book();
+    return is;
+}
+
+std::ostream& operator<<(std::ostream& os, const Book& book)
+{
+    os<<book.no_<<" "<<book.name_<<" "<<book.author_<<" "<<book.pubdate_<<" "<<book.number_;
+    return os;
 }
 
 bool operator==(const Book& lhs, const Book& rhs)
 {
-    return lhs.no_ == rhs.no_;
+    return lhs.no_==rhs.no_;
 }
 
 bool operator!=(const Book& lhs, const Book& rhs)
 {
-    return !(lhs == rhs);
+    return !(lhs==rhs);
 }
 
 bool operator<(const Book& lhs, const Book& rhs)
 {
-    return lhs.no_ < rhs.no_;
+    return lhs.no_<rhs.no_;
 }
 
 bool operator>(const Book& lhs, const Book& rhs)
 {
-    return rhs < lhs;
+    return rhs<lhs;
 }
 
-Book& Book::operator+=(const Book& rhs)
+Book operator+(const Book& lhs, const Book&rhs)
 {
-    if (rhs == *this) this->number_ += rhs.number_;
-
-    return *this;
-}
-
-Book operator+(const Book& lhs, const Book& rhs)
-{
-    Book book = lhs;
-    book += rhs;
+    Book book=lhs;
+    book+=rhs;
     return book;
 }
