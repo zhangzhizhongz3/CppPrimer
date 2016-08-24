d5e4877a17e40ffd66b0d126635418b466bc56b6
zzz <zhangzhizhongz3@foxmail.com>
2016-08-24 19:17:23 +0800

    Update ex14_16_StrVec.h

--- ch14/ex14_16_StrVec.h
+++ ch14/ex14_16_StrVec.h
@@ -1,59 +1,54 @@
-#ifndef CP5_STRVEC_H_
-#define CP5_STRVEC_H_
+#ifndef StrVec_h
+#define StrVec_h
 
-#include <memory>
 #include <string>
+#include <memory>
+#include <utility>
+#include <cstddef>
 #include <initializer_list>
-
-#ifndef _MSC_VER
-#define NOEXCEPT noexcept
-#else
-#define NOEXCEPT
-#endif
+using namespace std;
 
 class StrVec {
-    friend bool operator==(const StrVec&, const StrVec&);
-    friend bool operator!=(const StrVec&, const StrVec&);
+friend bool operator==(const StrVec&, const StrVec&);
+friend bool operator!=(const StrVec&, const StrVec&);
 
 public:
-    StrVec() : elements(nullptr), first_free(nullptr), cap(nullptr) {}
-    StrVec(std::initializer_list<std::string>);
+    StrVec():elements(nullptr), first_free(nullptr), cap(nullptr) {}
+    StrVec(initializer_list<string>);
+
     StrVec(const StrVec&);
     StrVec& operator=(const StrVec&);
-    StrVec(StrVec&&) NOEXCEPT;
-    StrVec& operator=(StrVec&&) NOEXCEPT;
+    StrVec(StrVec&&) noexcept;
+    StrVec& operator=(StrVec&&) noexcept;
     ~StrVec();
 
-    void push_back(const std::string&);
-    size_t size() const { return first_free - elements; }
-    size_t capacity() const { return cap - elements; }
-    std::string* begin() const { return elements; }
-    std::string* end() const { return first_free; }
+    void push_back(const string&);
 
-    std::string& at(size_t pos) { return *(elements + pos); }
-    const std::string& at(size_t pos) const { return *(elements + pos); }
+    string* begin() const {return elements;}
+    string* end() const {return first_free;}
 
-    void reserve(size_t new_cap);
-    void resize(size_t count);
-    void resize(size_t count, const std::string&);
+    size_t size() const {return first_free-elements;}
+    size_t capacity() const {return cap-elements;}
+
+    string& at(size_t pos) {return *(elements+pos);}
+    const string& at(size_t pos) const {return *(elements+pos);}
+
+    void reserve(size_t);
+    void resize(size_t);
+    void resize(size_t, const string&);
 
 private:
-    std::pair<std::string*, std::string*> alloc_n_copy(const std::string*,
-                                                       const std::string*);
+    pair<string*, string*> alloc_n_copy(const string*, const string*);
+    void range_initialize(const string*, const string*);
     void free();
-    void chk_n_alloc()
-    {
-        if (size() == capacity()) reallocate();
-    }
     void reallocate();
-    void alloc_n_move(size_t new_cap);
-    void range_initialize(const std::string*, const std::string*);
+    void alloc_n_move(size_t);
+    void chk_n_alloc() {if(first_free==cap) reallocate();}
 
-private:
-    std::string* elements;
-    std::string* first_free;
-    std::string* cap;
-    std::allocator<std::string> alloc;
+    string* elements;
+    string* first_free;
+    string* cap;
+    allocator<string> alloc;
 };
 
 bool operator==(const StrVec&, const StrVec&);
