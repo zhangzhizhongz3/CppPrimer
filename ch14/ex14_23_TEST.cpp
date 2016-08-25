8b4552128fd8a30b6b38f877dda40da7124c976a
zzz <zhangzhizhongz3@foxmail.com>
2016-08-25 22:09:02 +0800

    Update ex14_23_TEST.cpp

--- ch14/ex14_23_TEST.cpp
+++ ch14/ex14_23_TEST.cpp
@@ -1,15 +1,16 @@
-#include "ex14_23.h"
 #include <iostream>
 #include <vector>
+#include "ex14_23.h"
+using namespace std;
 
 int main()
 {
     StrVec vec;
     vec.reserve(6);
-    std::cout << "capacity(reserve to 6): " << vec.capacity() << std::endl;
+    cout << "capacity(reserve to 6): " << vec.capacity() << endl;
 
     vec.reserve(4);
-    std::cout << "capacity(reserve to 4): " << vec.capacity() << std::endl;
+    cout << "capacity(reserve to 4): " << vec.capacity() << endl;
 
     vec.push_back("hello");
     vec.push_back("world");
@@ -17,29 +18,29 @@ int main()
     vec.resize(4);
 
     for (auto i = vec.begin(); i != vec.end(); ++i)
-        std::cout << *i << std::endl;
-    std::cout << "-EOF-" << std::endl;
+        cout << *i << endl;
+    cout << "-EOF-" << endl;
 
     vec.resize(1);
 
     for (auto i = vec.begin(); i != vec.end(); ++i)
-        std::cout << *i << std::endl;
-    std::cout << "-EOF-" << std::endl;
+        cout << *i << endl;
+    cout << "-EOF-" << endl;
 
     StrVec vec_list{"hello", "world", "pezy"};
 
     for (auto i = vec_list.begin(); i != vec_list.end(); ++i)
-        std::cout << *i << " ";
-    std::cout << std::endl;
+        cout << *i << " ";
+    cout << endl;
 
     // Test operator==
-
-    const StrVec const_vec_list = {"hello", "world", "pezy"};
+    const StrVec const_vec_list{"hello", "world", "pezy"};
     if (vec_list == const_vec_list)
-        for (const auto& str : const_vec_list) std::cout << str << " ";
-    std::cout << std::endl;
+        for (const auto& str : const_vec_list)
+            cout << str << " ";
+    cout << endl;
 
     // Test operator<
-    const StrVec const_vec_list_small = {"hello", "pezy", "ok"};
-    std::cout << (const_vec_list_small < const_vec_list) << std::endl;
+    const StrVec const_vec_list_small{"hello", "pezy", "ok"};
+    cout << boolalpha<<(const_vec_list_small < const_vec_list) << endl;
 }
