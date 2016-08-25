a5281f1ba6c56261e237ff9e264e1816726fb173
zzz <zhangzhizhongz3@foxmail.com>
2016-08-25 21:08:37 +0800

    Update ex14_18_StrBlobTest.cpp

--- ch14/ex14_18_StrBlobTest.cpp
+++ ch14/ex14_18_StrBlobTest.cpp
@@ -1,15 +1,16 @@
-#include "ex14_18_StrBlob.h"
 #include <iostream>
+#include "ex14_18_StrBlob.h"
+using namespace std;
 
 int main()
 {
     StrBlob sb1{"a", "b", "c"};
     StrBlob sb2{"a", "b", "b"};
 
-    if (sb1 > sb2) {
-        for (ConstStrBlobPtr iter = sb1.cbegin(); iter < sb1.cend();
-             iter.incr())
-            std::cout << iter.deref() << " ";
-        std::cout << std::endl;
+    if (sb1 > sb2)
+    {
+        for (ConstStrBlobPtr iter = sb1.cbegin(); iter < sb1.cend(); iter.incr())
+            cout << iter.deref() << " ";
+        cout << endl;
     }
 }
