e90abd6b0639d72a6838e7bc1739f914f6dc4ab1
zzz <zhangzhizhongz3@foxmail.com>
2016-08-25 21:56:33 +0800

    Update ex14_22.cpp

--- ch14/ex14_22.cpp
+++ ch14/ex14_22.cpp
@@ -1,8 +1,15 @@
+#include <iostream>
 #include "ex14_22.h"
 
 Sales_data::Sales_data(std::istream& is) : Sales_data()
 {
-    is >> *this;
+    is>>*this;
+}
+
+Sales_data& Sales_data::operator=(const std::string& isbn)
+{
+    *this=Sales_data(isbn);
+    return *this;
 }
 
 Sales_data& Sales_data::operator+=(const Sales_data& rhs)
@@ -14,31 +21,24 @@ Sales_data& Sales_data::operator+=(const Sales_data& rhs)
 
 std::istream& operator>>(std::istream& is, Sales_data& item)
 {
-    double price = 0.0;
-    is >> item.bookNo >> item.units_sold >> price;
-    if (is)
-        item.revenue = price * item.units_sold;
+    double price=0;
+    is>>item.bookNo>>item.units_sold>>price;
+    if(is)
+        item.revenue=price * item.units_sold;
     else
-        item = Sales_data();
+        item=Sales_data();
     return is;
 }
 
 std::ostream& operator<<(std::ostream& os, const Sales_data& item)
 {
-    os << item.isbn() << " " << item.units_sold << " " << item.revenue << " "
-       << item.avg_price();
+    os << item.isbn() << " " << item.units_sold << " " << item.revenue<<" "<<item.avg_price();
     return os;
 }
 
 Sales_data operator+(const Sales_data& lhs, const Sales_data& rhs)
 {
     Sales_data sum = lhs;
-    sum += rhs;
+    sum+=rhs;
     return sum;
-}
-
-Sales_data& Sales_data::operator=(const std::string& isbn)
-{
-    *this = Sales_data(isbn);
-    return *this;
 }
