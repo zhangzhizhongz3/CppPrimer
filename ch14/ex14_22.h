4d4d655532359e039b5b8b38be348b2c81607141
zzz <zhangzhizhongz3@foxmail.com>
2016-08-25 21:56:02 +0800

    Update ex14_22.h

--- ch14/ex14_22.h
+++ ch14/ex14_22.h
@@ -1,16 +1,3 @@
-/*
-=================================================================================
-
-C++ Primer 5th Exercise Answer Source Code
-Copyright (C) 2014-2015 github.com/pezy/Cpp-Primer
-
-Sales_data
-
-If you have questions, try to connect with me: pezy<urbancpz@gmail.com>
-
-=================================================================================
-*/
-
 #ifndef CP5_ex14_22_h
 #define CP5_ex14_22_h
 
@@ -18,24 +5,19 @@ If you have questions, try to connect with me: pezy<urbancpz@gmail.com>
 #include <iostream>
 
 class Sales_data {
-    friend std::istream& operator>>(std::istream&, Sales_data&);
-    friend std::ostream& operator<<(std::ostream&, const Sales_data&);
-    friend Sales_data operator+(const Sales_data&, const Sales_data&);
+friend std::istream& operator>>(std::istream&, Sales_data&);
+friend std::ostream& operator<<(std::ostream&, const Sales_data&);
+friend Sales_data operator+(const Sales_data&, const Sales_data&);
 
 public:
-    Sales_data(const std::string& s, unsigned n, double p)
-        : bookNo(s), units_sold(n), revenue(n * p)
-    {
-    }
-    Sales_data() : Sales_data("", 0, 0.0f) {}
-    Sales_data(const std::string& s) : Sales_data(s, 0, 0.0f) {}
-    Sales_data(std::istream& is);
+    Sales_data(const std::string& s, unsigned n, double p) : bookNo(s), units_sold(n), revenue(p * n) {}
+    Sales_data() : Sales_data("", 0, 0) {}
+    Sales_data(const std::string& s) : Sales_data(s, 0, 0) {}
+    Sales_data(std::istream&);
 
-    Sales_data& operator=(const std::string&);
-
-    Sales_data& operator+=(const Sales_data&);
     std::string isbn() const { return bookNo; }
-
+    Sales_data& operator=(const std::string&);
+    Sales_data& operator+=(const Sales_data&);
 private:
     inline double avg_price() const;
 
@@ -44,13 +26,13 @@ private:
     double revenue = 0.0;
 };
 
-std::istream& operator>>(std::istream&, Sales_data&);
-std::ostream& operator<<(std::ostream&, const Sales_data&);
-Sales_data operator+(const Sales_data&, const Sales_data&);
-
-inline double Sales_data::avg_price() const
+double Sales_data::avg_price() const
 {
     return units_sold ? revenue / units_sold : 0;
 }
+
+std::istream& operator>>(std::istream&, Sales_data&);
+std::ostream& operator<<(std::ostream&, const Sales_data&);
+Sales_data operator+(const Sales_data&, const Sales_data&);
 
 #endif
