c8938c39a7fdbc6d880527a11b1a287d440d1f59
zzz <zhangzhizhongz3@foxmail.com>
2016-08-26 15:48:14 +0800

    Update ex14_24.h

--- ch14/ex14_24.h
+++ ch14/ex14_24.h
@@ -1,140 +1,131 @@
-#ifndef DATE_H
-#define DATE_H
-
-#ifndef _MSC_VER
-#define NOEXCEPT noexcept
-#else
-#define NOEXCEPT
-#endif
+#ifndef Date_h
+#define Date_h
 
 #include <iostream>
+#include <cstddef>
 #include <vector>
+using namespace std;
 
 class Date {
-    friend bool operator==(const Date& lhs, const Date& rhs);
-    friend bool operator<(const Date& lhs, const Date& rhs);
-    friend bool check(const Date& d);
-    friend std::ostream& operator<<(std::ostream& os, const Date& d);
+//! friend
+friend ostream& operator<<(ostream&, const Date&);
+friend bool operator==(const Date&, const Date&);
+friend bool operator<(const Date&, const Date&);
+friend bool check(const Date&);
 
 public:
-    typedef std::size_t Size;
-
+    //! constructors
     //! default constructor
-    Date() = default;
-    //! constructor taking Size as days
-    explicit Date(Size days);
-    //! constructor taking three Size
-    Date(Size d, Size m, Size y) : day(d), month(m), year(y) {}
+    Date()=default;
+    //! constructor taking size_t as days
+    explicit Date(size_t);
+    //! constructor taking three size_t
+    Date(size_t d, size_t m, size_t y):day(d), month(m), year(y) {}
     //! constructor taking iostream
-    Date(std::istream& is, std::ostream& os);
+    Date(istream&, ostream&);
 
     //! copy constructor
-    Date(const Date& d);
+    Date(const Date&);
+    //! copy-assignment operator
+    Date& operator=(const Date&);
     //! move constructor
-    Date(Date&& d) NOEXCEPT;
+    Date(Date&&) noexcept;
+    //! move-assignment operator
+    Date& operator=(Date&&) noexcept;
+    //! destructor -- in this case, user-defined destructor is not necessary
+    ~Date() {cout<<"destroying"<<endl;}
 
-    //! copy operator=
-    Date& operator=(const Date& d);
-    //! move operator=
-    Date& operator=(Date&& rhs) NOEXCEPT;
-
-    //! destructor  --  in this case, user-defined destructor is not nessary.
-    ~Date() { std::cout << "destroying\n"; }
-
-    //! members
-    Size toDays() const; // not implemented yet.
-    Date& operator+=(Size offset);
-    Date& operator-=(Size offset);
+    //! member functions
+    size_t toDays() const;  //not implemented yet
+    Date& operator+=(size_t);
+    Date& operator-=(size_t);
 
 private:
-    Size day = 1;
-    Size month = 1;
-    Size year = 0;
+    //! tool functions
+
+    //! data members
+    size_t day=1;
+    size_t month=1;
+    size_t year=0;
 };
 
-static const Date::Size YtoD_400 = 146097; // 365*400 + 400/4 -3 == 146097
-static const Date::Size YtoD_100 = 36524; // 365*100 + 100/4 -1 ==  36524
-static const Date::Size YtoD_4 = 1461; // 365*4 + 1          ==   1461
-static const Date::Size YtoD_1 = 365; // 365
+static const size_t YtoD_400=146097;    //365*400=400/4-3==146097
+static const size_t YtoD_100=36524;     //365*100+100/4-1==36524
+static const size_t YtoD_4=1461;        //365*4+1        ==1461
+static const size_t YtoD_1=365;         //365
 
 //! normal year
-static const std::vector<Date::Size> monthsVec_n = {31, 28, 31, 30, 31, 30,
-                                                    31, 31, 30, 31, 30, 31};
+static const vector<size_t> monthsVec_n={31,28,31,30,31,30,31,31,30,31,30,31};
 
 //! leap year
-static const std::vector<Date::Size> monthsVec_l = {31, 29, 31, 30, 31, 30,
-                                                    31, 31, 30, 31, 30, 31};
+static const vector<size_t> monthsVec_l={31,29,31,30,31,30,31,31,30,31,30,31};
 
-//! non-member operators:  <<  >>  -   ==  !=  <   <=  >   >=
-//!
-std::ostream& operator<<(std::ostream& os, const Date& d);
-std::istream& operator>>(std::istream& is, Date& d);
-int operator-(const Date& lhs, const Date& rhs);
-bool operator==(const Date& lhs, const Date& rhs);
-bool operator!=(const Date& lhs, const Date& rhs);
-bool operator<(const Date& lhs, const Date& rhs);
-bool operator<=(const Date& lhs, const Date& rhs);
-bool operator>(const Date& lhs, const Date& rhs);
-bool operator>=(const Date& lhs, const Date& rhs);
-Date operator-(const Date& lhs, Date::Size rhs);
-Date operator+(const Date& lhs, Date::Size rhs);
+istream& operator>>(istream&, Date&);
+ostream& operator<<(ostream&, const Date&);
+bool operator==(const Date&, const Date&);
+bool operator!=(const Date&, const Date&);
+bool operator<(const Date&, const Date&);
+bool operator>(const Date&, const Date&);
+bool operator<=(const Date&, const Date&);
+bool operator>=(const Date&, const Date&);
+int operator-(const Date&, const Date&);
+Date operator-(const Date&, size_t);
+Date operator+(const Date&, size_t);
 
-//!  utillities:
-bool check(const Date& d);
-inline bool isLeapYear(Date::Size y);
+//! utilities
+bool check(const Date&);
+inline bool isLeapYear(size_t);
 
 //! check if the date object passed in is valid
 inline bool check(const Date& d)
 {
-    if (d.month == 0 || d.month > 12)
+    if(d.month==0||d.month>12)
         return false;
-    else {
-        //!    month == 1 3 5 7 8 10 12
-        if (d.month == 1 || d.month == 3 || d.month == 5 || d.month == 7 ||
-            d.month == 8 || d.month == 10 || d.month == 12) {
-            if (d.day == 0 || d.day > 31)
+    else
+    {
+        //! month==1 3 5 7 8 10 12
+        if(d.month==1||d.month==3||d.month==5||d.month==7||d.month==8||d.month==10||d.month==12)
+        {
+            if(d.day==0||d.day>31)
                 return false;
             else
                 return true;
         }
-        else {
-            //!    month == 4 6 9 11
-            if (d.month == 4 || d.month == 6 || d.month == 9 || d.month == 11) {
-                if (d.day == 0 || d.day > 30)
+        else
+        {
+            //! month==4 6 9 11
+            if(d.month==4||d.month==6||d.month==9||d.month==11)
+            {
+                if(d.day==0||d.day>30)
                     return false;
                 else
                     return true;
             }
-            else {
-                //!    month == 2
-                if (isLeapYear(d.year)) {
-                    if (d.day == 0 || d.day > 29)
+            else
+            {
+                //! month==2
+                if(isLeapYear(d.year))
+                {
+                    if(d.day==0||d.day>29)
                         return false;
                     else
                         return true;
                 }
-                else {
-                    if (d.day == 0 || d.day > 28)
+                else
+                {
+                    if(d.day==0||d.day>28)
                         return false;
                     else
                         return true;
                 }
             }
-        }
+         }
     }
 }
 
-inline bool isLeapYear(Date::Size y)
+inline bool isLeapYear(size_t y)
 {
-    if (!(y % 400)) {
-        return true;
-    }
-    else {
-        if (!(y % 100)) {
-            return false;
-        }
-        else
-            return !(y % 4);
-    }
+    return ((y%4==0&&y%100!=0)||(y%400==0));
 }
-#endif // DATE_H
+
+#endif
