94fe00c3fec1dd6d194ad8d66f18a98db4b41ef5
zzz <zhangzhizhongz3@foxmail.com>
2016-08-26 15:49:03 +0800

    Update ex14_24.cpp

--- ch14/ex14_24.cpp
+++ ch14/ex14_24.cpp
@@ -1,37 +1,38 @@
-#include "ex14_24.h"
+#include <iostream>
+#include <cstddef>
+#include <vector>
 #include <algorithm>
+#include "ex14_24.h"
 
-//! constructor taking Size as days
+//! constructor taking size_t as days
 //! the argument must be within (0, 2^32)
-Date::Date(Size days)
+Date::Date(size_t days)
 {
     //! calculate the year
-    Size y400 = days / YtoD_400;
-    Size y100 = (days - y400 * YtoD_400) / YtoD_100;
-    Size y4 = (days - y400 * YtoD_400 - y100 * YtoD_100) / YtoD_4;
-    Size y = (days - y400 * YtoD_400 - y100 * YtoD_100 - y4 * YtoD_4) / 365;
-    Size d = days - y400 * YtoD_400 - y100 * YtoD_100 - y4 * YtoD_4 - y * 365;
-    this->year = y400 * 400 + y100 * 100 + y4 * 4 + y;
+    size_t y400 = days / YtoD_400;
+    size_t y100 = (days - y400 * YtoD_400) / YtoD_100;
+    size_t y4 = (days - y400 * YtoD_400 - y100 * YtoD_100) / YtoD_4;
+    size_t y = (days - y400 * YtoD_400 - y100 * YtoD_100 - y4 * YtoD_4) / 365;
+    size_t d = days - y400 * YtoD_400 - y100 * YtoD_100 - y4 * YtoD_4 - y * 365;
+    year = y400 * 400 + y100 * 100 + y4 * 4 + y;
 
     //! check if leap and choose the months vector accordingly
-    std::vector<Size> currYear =
-        isLeapYear(this->year) ? monthsVec_l : monthsVec_n;
+    vector<size_t> currYear = isLeapYear(year) ? monthsVec_l : monthsVec_n;
 
     //! calculate day and month using find_if + lambda
-    Size D_accumu = 0, M_accumu = 0;
-    //! @bug    fixed:  the variabbles above hade been declared inside the
-    //! find_if as static
-    //!                 which caused the bug. It works fine now after being move
-    //!                 outside.
+    size_t D_accumu = 0, M_accumu = 0;
+    //! @bug    fixed:  the variables above had been declared inside the find_if as static
+    //!                 which caused the bug. It works fine now after being move outside.
 
-    std::find_if(currYear.cbegin(), currYear.cend(), [&](Size m) {
+    find_if(currYear.cbegin(), currYear.cend(), [&](size_t m) {
 
         D_accumu += m;
         M_accumu++;
 
-        if (d < D_accumu) {
-            this->month = M_accumu;
-            this->day = d + m - D_accumu;
+        if (d < D_accumu)
+        {
+            month = M_accumu;
+            day = d + m - D_accumu;
 
             return true;
         }
@@ -40,126 +41,119 @@ Date::Date(Size days)
     });
 }
 
-//! construcotr taking iostream
-Date::Date(std::istream& is, std::ostream& os)
+//! constructor taking iostream
+Date::Date(istream& is, ostream& os)
 {
     is >> day >> month >> year;
 
-    if (is) {
+    if (is)
+    {
         if (check(*this))
             return;
-        else {
+        else
+        {
             os << "Invalid input! Object is default initialized.";
             *this = Date();
         }
     }
-    else {
+    else
+    {
         os << "Invalid input! Object is default initialized.";
         *this = Date();
     }
 }
 
 //! copy constructor
-Date::Date(const Date& d) : day(d.day), month(d.month), year(d.year)
+Date::Date(const Date& d) : day(d.day), month(d.month), year(d.year) {}
+
+//! copy-assignment operator=
+Date& Date::operator=(const Date& d)
 {
+    day = d.day;
+    month = d.month;
+    year = d.year;
+
+    return *this;
 }
 
 //! move constructor
-Date::Date(Date&& d) NOEXCEPT : day(d.day), month(d.month), year(d.year)
+Date::Date(Date&& d) noexcept : day(d.day), month(d.month), year(d.year)
 {
-    std::cout << "copy moving";
+    cout << "copy moving";
 }
 
-//! copy operator=
-Date& Date::operator=(const Date& d)
+//! move-assignment operator=
+Date& Date::operator=(Date&& rhs) noexcept
 {
-    this->day = d.day;
-    this->month = d.month;
-    this->year = d.year;
-
-    return *this;
-}
-
-//! move operator=
-Date& Date::operator=(Date&& rhs) NOEXCEPT
-{
-    if (this != &rhs) {
-        this->day = rhs.day;
-        this->month = rhs.month;
-        this->year = rhs.year;
+    if (this != &rhs)
+    {
+        day = rhs.day;
+        month = rhs.month;
+        year = rhs.year;
     }
-    std::cout << "moving =";
+    cout << "moving =";
 
     return *this;
 }
 
-//! conver to days
-Date::Size Date::toDays() const
+//! convert to days
+size_t Date::toDays() const
 {
-    Size result = this->day;
+    size_t result = day;
 
     //! check if leap and choose the months vector accordingly
-    std::vector<Size> currYear =
-        isLeapYear(this->year) ? monthsVec_l : monthsVec_n;
+    vector<size_t> currYear = isLeapYear(year) ? monthsVec_l : monthsVec_n;
 
     //! calculate result + days by months
-    for (auto it = currYear.cbegin(); it != currYear.cbegin() + this->month - 1;
-         ++it)
+    for (auto it = currYear.cbegin(); it != currYear.cbegin() + month - 1; ++it)
         result += *it;
 
     //! calculate result + days by years
-    result += (this->year / 400) * YtoD_400;
-    result += (this->year % 400 / 100) * YtoD_100;
-    result += (this->year % 100 / 4) * YtoD_4;
-    result += (this->year % 4) * YtoD_1;
+    result += (year / 400) * YtoD_400;
+    result += (year % 400 / 100) * YtoD_100;
+    result += (year % 100 / 4) * YtoD_4;
+    result += (year % 4) * YtoD_1;
 
     return result;
 }
 
 //! member operators:   +=  -=
-
-Date& Date::operator+=(Date::Size offset)
+Date& Date::operator+=(size_t offset)
 {
-    *this = Date(this->toDays() + offset);
+    *this = Date(toDays() + offset);
     return *this;
 }
 
-Date& Date::operator-=(Date::Size offset)
+Date& Date::operator-=(size_t offset)
 {
-    if (this->toDays() > offset)
-        *this = Date(this->toDays() - offset);
+    if (toDays() > offset)
+        *this = Date(toDays() - offset);
     else
         *this = Date();
 
     return *this;
 }
 
-//! non-member operators:  <<  >>  -   ==  !=  <   <=  >   >=
-
-std::ostream& operator<<(std::ostream& os, const Date& d)
+//! non-member operators:  >>  <<  ==  !=  <   >  <=   >=  -  -  +
+istream& operator>>(istream& is, Date& d)
 {
-    os << d.day << " " << d.month << " " << d.year;
-    return os;
-}
-
-std::istream& operator>>(std::istream& is, Date& d)
-{
-    if (is) {
-        Date input = Date(is, std::cout);
+    if (is)
+    {
+        Date input = Date(is, cout);
         if (check(input)) d = input;
     }
     return is;
 }
 
-int operator-(const Date& lhs, const Date& rhs)
+ostream& operator<<(ostream& os, const Date& d)
 {
-    return lhs.toDays() - rhs.toDays();
+    os << d.day << " " << d.month << " " << d.year;
+    return os;
 }
 
 bool operator==(const Date& lhs, const Date& rhs)
 {
-    return (lhs.day == rhs.day) && (lhs.month == rhs.month) &&
-           (lhs.year == rhs.year);
+    return (lhs.day == rhs.day) && (lhs.month == rhs.month) && (lhs.year == rhs.year);
 }
 
 bool operator!=(const Date& lhs, const Date& rhs)
@@ -172,14 +166,14 @@ bool operator<(const Date& lhs, const Date& rhs)
     return lhs.toDays() < rhs.toDays();
 }
 
-bool operator<=(const Date& lhs, const Date& rhs)
-{
-    return (lhs < rhs) || (lhs == rhs);
-}
-
 bool operator>(const Date& lhs, const Date& rhs)
 {
     return !(lhs <= rhs);
+}
+
+bool operator<=(const Date& lhs, const Date& rhs)
+{
+    return (lhs < rhs) || (lhs == rhs);
 }
 
 bool operator>=(const Date& lhs, const Date& rhs)
@@ -187,7 +181,12 @@ bool operator>=(const Date& lhs, const Date& rhs)
     return !(lhs < rhs);
 }
 
-Date operator-(const Date& lhs, Date::Size rhs)
+int operator-(const Date& lhs, const Date& rhs)
+{
+    return lhs.toDays() - rhs.toDays();
+}
+
+Date operator-(const Date& lhs, size_t rhs)
 { //!  ^^^ rhs must not be larger than 2^32-1
     //! copy lhs
     Date result(lhs);
@@ -196,7 +195,7 @@ Date operator-(const Date& lhs, Date::Size rhs)
     return result;
 }
 
-Date operator+(const Date& lhs, Date::Size rhs)
+Date operator+(const Date& lhs, size_t rhs)
 { //!  ^^^ rhs must not be larger than 2^32-1
     //! copy lhs
     Date result(lhs);
