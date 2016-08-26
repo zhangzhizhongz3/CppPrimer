1fd8b2c7d0a5b93ffb286547bf501ca7b2049cc4
zzz <zhangzhizhongz3@foxmail.com>
2016-08-26 16:17:01 +0800

    Update ex14_26_StrVec.cpp

--- ch14/ex14_26_StrVec.cpp
+++ ch14/ex14_26_StrVec.cpp
@@ -1,43 +1,77 @@
+#include <string>
+#include <memory>
+#include <utility>
+#include <cstddef>
+#include <algorithm>
+#include <initializer_list>
 #include "ex14_26_StrVec.h"
-#include <algorithm> // for_each, equal
+using namespace std;
 
-void StrVec::push_back(const std::string& s)
+bool operator==(const StrVec& lhs, const StrVec& rhs)
 {
-    chk_n_alloc();
-    alloc.construct(first_free++, s);
+    return ((lhs.size()==rhs.size())&&equal(lhs.begin(), lhs.end(), rhs.begin()));
 }
 
-std::pair<std::string*, std::string*> StrVec::alloc_n_copy(const std::string* b,
-                                                           const std::string* e)
+bool operator!=(const StrVec& lhs, const StrVec& rhs)
 {
-    auto data = alloc.allocate(e - b);
-    return {data, std::uninitialized_copy(b, e, data)};
+    return !(lhs==rhs);
 }
 
-void StrVec::free()
+bool operator<(const StrVec& lhs, const StrVec& rhs)
 {
-    if (elements) {
-        for_each(elements, first_free,
-                 [this](std::string& rhs) { alloc.destroy(&rhs); });
-        alloc.deallocate(elements, cap - elements);
-    }
+    return lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
 }
 
-void StrVec::range_initialize(const std::string* first, const std::string* last)
+bool operator>(const StrVec& lhs, const StrVec& rhs)
 {
-    auto newdata = alloc_n_copy(first, last);
-    elements = newdata.first;
-    first_free = cap = newdata.second;
+    return rhs<lhs;
 }
 
-StrVec::StrVec(const StrVec& rhs)
+bool operator<=(const StrVec& lhs, const StrVec& rhs)
 {
-    range_initialize(rhs.begin(), rhs.end());
+    return !(rhs<lhs);
 }
 
-StrVec::StrVec(std::initializer_list<std::string> il)
+bool operator>=(const StrVec& lhs, const StrVec& rhs)
+{
+    return !(lhs<rhs);
+}
+
+StrVec::StrVec(initializer_list<string> il)
 {
     range_initialize(il.begin(), il.end());
+}
+
+StrVec::StrVec(const StrVec& s)
+{
+    range_initialize(s.begin(), s.end());
+}
+
+StrVec& StrVec::operator=(const StrVec& rhs)
+{
+    auto data=alloc_n_copy(rhs.begin(), rhs.end());
+    free();
+    elements=data.first;
+    first_free=cap=data.second;
+    return *this;
+}
+
+StrVec::StrVec(StrVec&& s) noexcept : elements(s.elements), first_free(s.first_free), cap(s.cap)
+{
+    s.elements=s.first_free=s.cap=nullptr;
+}
+
+StrVec& StrVec::operator=(StrVec&& rhs) noexcept
+{
+    if(this!=&rhs)
+    {
+        free();
+        elements=rhs.elements;
+        first_free=rhs.first_free;
+        cap=rhs.cap;
+        rhs.elements=rhs.first_free=rhs.cap=nullptr;
+    }
+    return *this;
 }
 
 StrVec::~StrVec()
@@ -45,105 +79,77 @@ StrVec::~StrVec()
     free();
 }
 
-StrVec& StrVec::operator=(const StrVec& rhs)
+void StrVec::push_back(const string& s)
 {
-    auto data = alloc_n_copy(rhs.begin(), rhs.end());
-    free();
-    elements = data.first;
-    first_free = cap = data.second;
-    return *this;
-}
-
-void StrVec::alloc_n_move(size_t new_cap)
-{
-    auto newdata = alloc.allocate(new_cap);
-    auto dest = newdata;
-    auto elem = elements;
-    for (size_t i = 0; i != size(); ++i)
-        alloc.construct(dest++, std::move(*elem++));
-    free();
-    elements = newdata;
-    first_free = dest;
-    cap = elements + new_cap;
-}
-
-void StrVec::reallocate()
-{
-    auto newcapacity = size() ? 2 * size() : 1;
-    alloc_n_move(newcapacity);
+    chk_n_alloc();
+    alloc.construct(first_free++, s);
 }
 
 void StrVec::reserve(size_t new_cap)
 {
-    if (new_cap <= capacity()) return;
+    if(new_cap<=capacity())
+        return;
     alloc_n_move(new_cap);
 }
 
 void StrVec::resize(size_t count)
 {
-    resize(count, std::string());
+    resize(count, string());
 }
 
-void StrVec::resize(size_t count, const std::string& s)
+void StrVec::resize(size_t count, const string& s)
 {
-    if (count > size()) {
-        if (count > capacity()) reserve(count * 2);
-        for (size_t i = size(); i != count; ++i)
+    if(count>size())
+    {
+        if(count>capacity())
+            reserve(2*count);
+        for(size_t i=size(); i!=count; ++i)
             alloc.construct(first_free++, s);
     }
-    else if (count < size()) {
-        while (first_free != elements + count) alloc.destroy(--first_free);
+    else if(count<size())
+    {
+        while(first_free!=elements+count)
+            alloc.destroy(--first_free);
     }
 }
 
-StrVec::StrVec(StrVec&& s) NOEXCEPT : elements(s.elements),
-                                      first_free(s.first_free),
-                                      cap(s.cap)
+pair<string*, string*> StrVec::alloc_n_copy(const string* b, const string* e)
 {
-    // leave s in a state in which it is safe to run the destructor.
-    s.elements = s.first_free = s.cap = nullptr;
+    auto data=alloc.allocate(e-b);
+    return {data, uninitialized_copy(b, e, data)};
 }
 
-StrVec& StrVec::operator=(StrVec&& rhs) NOEXCEPT
+void StrVec::range_initialize(const string* first, const string* last)
 {
-    if (this != &rhs) {
-        free();
-        elements = rhs.elements;
-        first_free = rhs.first_free;
-        cap = rhs.cap;
-        rhs.elements = rhs.first_free = rhs.cap = nullptr;
+    auto newdata=alloc_n_copy(first, last);
+    elements=newdata.first;
+    first_free=cap=newdata.second;
+}
+
+void StrVec::free()
+{
+    if(elements)
+    {
+        for_each(elements, first_free, [this](string& s) {alloc.destroy(&s);});
+        alloc.deallocate(elements, cap-elements);
     }
-    return *this;
 }
 
-bool operator==(const StrVec& lhs, const StrVec& rhs)
+void StrVec::reallocate()
 {
-    return (lhs.size() == rhs.size() &&
-            std::equal(lhs.begin(), lhs.end(), rhs.begin()));
+    auto newcapacity=size() ? 2*size() : 1;
+    alloc_n_move(newcapacity);
 }
 
-bool operator!=(const StrVec& lhs, const StrVec& rhs)
+void StrVec::alloc_n_move(size_t new_cap)
 {
-    return !(lhs == rhs);
-}
-
-bool operator<(const StrVec& lhs, const StrVec& rhs)
-{
-    return std::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(),
-                                        rhs.end());
-}
-
-bool operator>(const StrVec& lhs, const StrVec& rhs)
-{
-    return rhs < lhs;
-}
-
-bool operator<=(const StrVec& lhs, const StrVec& rhs)
-{
-    return !(rhs < lhs);
-}
-
-bool operator>=(const StrVec& lhs, const StrVec& rhs)
-{
-    return !(lhs < rhs);
+    auto newdata=alloc.allocate(new_cap);
+    auto dest=newdata;
+    auto elem=elements;
+    for(size_t i=0; i!=size(); ++i)
+        alloc.construct(dest++, std::move(*elem++));
+    free();
+    elements=newdata;
+    first_free=dest;
+    cap=elements+new_cap;
 }
