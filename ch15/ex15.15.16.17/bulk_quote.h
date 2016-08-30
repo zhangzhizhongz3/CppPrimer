d255ae5183f12d4821fc3d3baa445636698f4a0d
zzz <zhangzhizhongz3@foxmail.com>
2016-08-30 21:47:05 +0800

    Update bulk_quote.h

--- ch15/ex15.15.16.17/bulk_quote.h
+++ ch15/ex15.15.16.17/bulk_quote.h
@@ -1,18 +1,17 @@
-#ifndef BULK_QUOTE_H
-#define BULK_QUOTE_H
-#include "disc_quote.h"
+#ifndef Bulk_quote_h
+#define Bulk_quote_h
 
-class Bulk_quote : public Disc_quote
-{
+#include <string>
+#include <cstddef>
+#include "Disc_quote.h"
+using namespace std;
+
+class Bulk_quote : public Disc_quote {
 public:
-    Bulk_quote() = default;
-    Bulk_quote(const std::string& b, double p, std::size_t q, double disc) :
-        Disc_quote(b,p,q,disc) {   }
-
-    double net_price(std::size_t n) const override;
-    void  debug() const override;
-
-
+    Bulk_quote()=default;
+    Bulk_quote(const string& b, double p, size_t q, double d) : Disc_quote(b, p, q, d) {}
+    double net_price(size_t) const override;
+    void debug() const override;
 };
 
-#endif // BULK_QUOTE_H
+#endif // Bulk_quote_h
