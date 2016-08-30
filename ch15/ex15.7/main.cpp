088f3e38fa5c3f13045339029800bd78c43d30fd
zzz <zhangzhizhongz3@foxmail.com>
2016-08-30 16:55:52 +0800

    Update main.cpp

--- ch15/ex15.7/main.cpp
+++ ch15/ex15.7/main.cpp
@@ -1,10 +1,3 @@
-/***************************************************************************
- *  @file       main.cpp
- *  @author     Alan.W
- *  @date       21  Jan 2014
- *  @remark     This code is for the exercises from C++ Primer 5th Edition
- *  @note
- ***************************************************************************/
 //!
 //! Exercise 15.7:
 //! Define a class that implements a limited discount strategy, which applies
@@ -14,39 +7,29 @@
 //!
 
 #include <iostream>
-#include <string>
+#include <cstddef>
+#include "Quote.h"
+#include "Bulk_quote.h"
+#include "Limit_quote.h"
+using namespace std;
 
-#include "quote.h"
-#include "bulk_quote.h"
-#include "limit_quote.h"
+double print_total(ostream& os, const Quote& item, size_t n);
 
-double print_total (std::ostream& os, const Quote& item, size_t n);
 int main()
 {
-    //! ex15.6
     Quote q("textbook", 10.60);
-    Bulk_quote bq("textbook", 10.60, 10, 0.3);
-    Limit_quote lq("Bible", 10.60, 10 , 0.3);
+    Bulk_quote bq("textbook", 10.6, 10, 0.3);
+    Limit_quote lq("Bible", 10.6, 10, 0.3);
 
-    print_total(std::cout, q, 5);
-    print_total(std::cout, bq, 5);
-    print_total(std::cout ,lq, 5);
-
-    return 0;
+    print_total(cout, q, 5);
+    print_total(cout, bq, 5);
+    print_total(cout, lq, 5);
 }
 
-double print_total(std::ostream &os, const Quote &item, size_t n)
+double print_total(ostream& os, const Quote& item, size_t n)
 {
-    double ret = item.net_price(n);
+    double ret=item.net_price(n);
 
-    os << "ISBN:" << item.isbn()
-       << "# sold: " << n << " total due: " << ret << std::endl;
-
+    os<<"ISBN: "<<item.isbn()<<" # sold: "<<" total due: "<<ret<<endl;
     return ret;
 }
-
-
-
-
-
-
