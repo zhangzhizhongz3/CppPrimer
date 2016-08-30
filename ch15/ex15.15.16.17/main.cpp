85ef15aab8ee7f0dc2a2358e62928df3e0778ee0
zzz <zhangzhizhongz3@foxmail.com>
2016-08-30 21:39:56 +0800

    Update main.cpp

--- ch15/ex15.15.16.17/main.cpp
+++ ch15/ex15.15.16.17/main.cpp
@@ -1,10 +1,3 @@
-/***************************************************************************
- *  @file       main.cpp
- *  @author     Alan.W
- *  @date       23  Jan 2014
- *  @remark     This code is for the exercises from C++ Primer 5th Edition
- *  @note
- ***************************************************************************/
 //!
 //! Exercise 15.15:
 //! Define your own versions of Disc_quote and Bulk_quote.
@@ -17,34 +10,15 @@
 //! Try to define an object of type Disc_quote and see what errors you get from
 //! the compiler.
 //  error: cannot declare variable 'd' to be of abstract type 'Disc_quote'
-//!           Disc_quote d;
-//!                      ^
-//  note:  because the following virtual functions are pure within 'Disc_quote':
-//!  class Disc_quote : public Quote
-//!        ^
-//  note: 	virtual double Disc_quote::net_price(std::size_t) const
-//!  virtual double net_price(std::size_t n) const override = 0;
 //!                 ^
 
-
-#include <iostream>
-#include <string>
-
-#include "quote.h"
-#include "bulk_quote.h"
-#include "limit_quote.h"
-#include "disc_quote.h"
-
-
+#include "Quote.h"
+#include "Disc_quote.h"
+#include "Bulk_quote.h"
+#include "Limit_quote.h"
+using namespace std;
 
 int main()
 {
-	/*
-	error C2259 : 'Disc_quote' : cannot instantiate abstract class
-	1>          due to following members :
-	1>          'double Disc_quote::net_price(size_t) const' : is abstract
-	*/
     Disc_quote d;
-
-    return 0;
 }
