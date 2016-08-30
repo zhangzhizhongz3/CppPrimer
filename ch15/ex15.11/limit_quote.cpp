0de5909e3e44ce598f38ab9f29aaaaa99221a89d
zzz <zhangzhizhongz3@foxmail.com>
2016-08-30 16:47:13 +0800

    Update limit_quote.cpp

--- ch15/ex15.11/limit_quote.cpp
+++ ch15/ex15.11/limit_quote.cpp
@@ -1,9 +1,18 @@
-#include "limit_quote.h"
+#include <iostream>
+#include <cstddef>
+#include "Limit_quote.h"
 
+double Limit_quote::net_price(size_t n) const
+{
+    if(n<=max_qty)
+        return n*price*(1-discount);
+    else
+        return max_qty*price*(1-discount)+(n-max_qty)*price;
+}
 
 void Limit_quote::debug() const
 {
-	Quote::debug();
-    std::cout << "max_qty= " << this->max_qty << " "
-              << "discount= " << this->discount<< " ";
+    Quote::debug();
+    cout<<"max_qty= "<<max_qty<<" "
+        <<"discount= "<<discount<<" ";
 }
