3df3e02c43ebabc5a2556f7e17ac1425634063ff
zzz <zhangzhizhongz3@foxmail.com>
2016-08-30 09:43:29 +0800

    Update limit_quote.cpp

--- ch15/ex15.7/limit_quote.cpp
+++ ch15/ex15.7/limit_quote.cpp
@@ -1,3 +1,10 @@
-#include "limit_quote.h"
+#include <cstddef>
+#include "Limit_quote.h"
 
-
+double Limit_quote::net_price(size_t n) const
+{
+    if(n<=max_qty)
+        return n*price*(1-discount);
+    else
+        return max_qty*price*(1-discount)+(n-max_qty)*price;
+}
