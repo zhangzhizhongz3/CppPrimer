6add7b6ea5cef6faf42fff248ecad6e7036e1725
zzz <zhangzhizhongz3@foxmail.com>
2016-08-31 21:37:56 +0800

    Update main.cpp

--- ch15/ex15.21.22/main.cpp
+++ ch15/ex15.21.22/main.cpp
@@ -1,10 +1,3 @@
-/***************************************************************************
- *  @file       main.cpp
- *  @author     Alan.W
- *  @date       23  Jan 2014
- *  @remark     This code is for the exercises from C++ Primer 5th Edition
- *  @note
- ***************************************************************************/
 //!
 //! Exercise 15.21:
 //! Choose one of the following general abstractions containing a family of
@@ -19,42 +12,31 @@
 //! likely virtual functions as well as public and protected members.
 //!
 
-#include <iostream>
+#include <utility>
 #include <string>
-
-#include "quote.h"
-#include "bulk_quote.h"
-#include "limit_quote.h"
-#include "disc_quote.h"
+using namespace std;
 
 //! just for 2D shape
-class Shape
-{
+class Shape {
 public:
-    typedef std::pair<double, double>    Coordinate;
+    typedef pair<double, double> Coordinate;
 
     Shape() = default;
-    Shape(const std::string& n) :
-        name(n) { }
+    Shape(const string& n) : name(n) { }
 
-    virtual double area()       const = 0;
-    virtual double perimeter()  const = 0;
+    virtual double area() const = 0;
+    virtual double perimeter() const = 0;
 
     virtual ~Shape() = default;
+
 private:
-    std::string name;
+    string name;
 };
 
-class Rectangle : public Shape
-{
+class Rectangle : public Shape {
 public:
     Rectangle() = default;
-    Rectangle(const std::string& n,
-              const Coordinate& a,
-              const Coordinate& b,
-              const Coordinate& c,
-              const Coordinate& d) :
-        Shape(n), a(a), b(b), c(c), d(d) { }
+    Rectangle(const string& n, const Coordinate& a, const Coordinate& b, const Coordinate& c,const Coordinate& d) : Shape(n), a(a), b(b), c(c), d(d) { }
 
     ~Rectangle() = default;
 
@@ -65,16 +47,10 @@ protected:
     Coordinate  d;
 };
 
-class Square : public Rectangle
-{
+class Square : public Rectangle {
 public:
     Square() = default;
-    Square(const std::string& n,
-           const Coordinate& a,
-           const Coordinate& b,
-           const Coordinate& c,
-           const Coordinate& d) :
-        Rectangle(n,a,b,c,d) { }
+    Square(const string& n, const Coordinate& a, const Coordinate& b, const Coordinate& c, const Coordinate& d) : Rectangle(n,a,b,c,d) { }
 
     ~Square() = default;
 };
@@ -82,6 +58,5 @@ public:
 
 int main()
 {
-
     return 0;
 }
