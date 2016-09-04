105bf532d9ef4794c97aef3d712083829fa23b0a
zzz <zhangzhizhongz3@foxmail.com>
2016-09-04 20:01:21 +0800

    Update bulk_quote.h

--- ch15/ex15.28.29/bulk_quote.h
+++ ch15/ex15.28.29/bulk_quote.h
@@ -1,62 +1,48 @@
-#ifndef BULK_QUOTE_H
-#define BULK_QUOTE_H
-#include "disc_quote.h"
+#ifndef Bulk_quote_h
+#define Bulk_quote_h
 
-class Bulk_quote : public Disc_quote
-{
+#include <string>
+#include <cstddef>
+#include <utility>
+#include "Disc_quote.h"
+using namespace std;
 
+class Bulk_quote : public Disc_quote {
+friend bool operator!=(const Bulk_quote&, const Bulk_quote&);
 public:
-    Bulk_quote() {std::cout << "default constructing Bulk_quote\n"; }
+    Bulk_quote() {};
+    //Bulk_quote(const string& b, double p, size_t q, double d) : Disc_quote(b, p, q, d) {cout<<"Bulk_quote: constructor taking 4 parameters"<<endl;}
 
-    //! changed the below to the inherited constructor for ex15.27.
-    //! rules:  1. only inherit from the direct base class.
-    //!         2. default,copy and move constructors can not inherit.
-    //!         3. any data members of its own are default initialized.
-    //!         4. the rest details are in the section section 15.7.4.
-    /*
-    Bulk_quote(const std::string& b, double p, std::size_t q, double disc) :
-        Disc_quote(b,p,q,disc) { std::cout << "Bulk_quote : constructor taking 4 parameters\n"; }
-    */
     using Disc_quote::Disc_quote;
 
     //! copy constructor
-    Bulk_quote(const Bulk_quote& bq) : Disc_quote(bq)
-    { std::cout << "Bulk_quote : copy constructor\n"; }
-
+    Bulk_quote(const Bulk_quote& bq) : Disc_quote(bq) {}
     //! move constructor
-    Bulk_quote(Bulk_quote&& bq) : Disc_quote(std::move(bq))
+    Bulk_quote(Bulk_quote&& bq) noexcept : Disc_quote(std::move(bq)) {}
+    //! copy =
+    Bulk_quote& operator=(const Bulk_quote& rhs)
     {
-        std::cout << "Bulk_quote : move constructor\n";
-    }
+        Disc_quote::operator=(rhs);
 
-    //! copy =()
-    Bulk_quote& operator =(const Bulk_quote& rhs)
+        return *this;
+    }
+    //! move =
+    Bulk_quote& operator=(Bulk_quote&& rhs) noexcept
     {
-        Disc_quote::operator =(rhs);
-        std::cout << "Bulk_quote : copy =()\n";
+        Disc_quote::operator=(std::move(rhs));
 
         return *this;
     }
 
+    double net_price(size_t) const override;
+    void debug() const override;
 
-    //! move =()
-    Bulk_quote& operator =(Bulk_quote&& rhs)
-    {
-        Disc_quote::operator =(std::move(rhs));
-        std::cout << "Bulk_quote : move =()\n";
-
-        return *this;
-    }
-
-    double net_price(std::size_t n) const override;
-    void  debug() const override;
-
-    ~Bulk_quote() override
-    {
-        std::cout << "destructing Bulk_quote\n";
-    }
+    virtual ~Bulk_quote() {}
 };
 
+inline bool operator!=(const Bulk_quote& lhs, const Bulk_quote& rhs)
+{
+    return Quote(lhs)!=Quote(rhs)||lhs.quantity!=rhs.quantity||lhs.discount!=rhs.discount;
+}
 
-
-#endif // BULK_QUOTE_H
+#endif // Bulk_quote_h
