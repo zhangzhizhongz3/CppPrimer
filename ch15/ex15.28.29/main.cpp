19d5e159bba1440440996323f9a8122f014f13de
zzz <zhangzhizhongz3@foxmail.com>
2016-09-04 20:03:56 +0800

    Update main.cpp

--- ch15/ex15.28.29/main.cpp
+++ ch15/ex15.28.29/main.cpp
@@ -19,49 +19,34 @@
 //!
 
 #include <iostream>
-#include <string>
 #include <vector>
 #include <memory>
-
-#include "quote.h"
-#include "bulk_quote.h"
-#include "limit_quote.h"
-#include "disc_quote.h"
-
+#include "Quote.h"
+#include "Disc_quote.h"
+#include "Bulk_quote.h"
+#include "Limit_quote.h"
+using namespace std;
 
 int main()
 {
-    /**
-     * @brief ex15.28   outcome == 9090
-     */
-    std::vector<Quote> v;
-    for(unsigned i =1; i != 10; ++i)
-        v.push_back(Bulk_quote("sss", i * 10.1, 10, 0.3));
+    //@brief ex15.28 outcome==9090
+    vector<Quote> v;
+    for(unsigned i=1; i!=10; ++i)
+        v.push_back(Bulk_quote("sss", i*10.1, 10, 0.3));
 
-    double total = 0;
-    for (const auto& b : v)
-    {
-        total += b.net_price(20);
-    }
-    std::cout << total << std::endl;
+    double total=0;
+    for(const auto& e : v)
+        total+=e.net_price(20);
+    cout<<total<<endl;
+    cout << "======================\n";
 
-    std::cout << "======================\n\n";
+    //@brief ex15.29 outcome==6363
+    vector<shared_ptr<Quote>> pv;
+    for(unsigned i=1; i!=10; ++i)
+        pv.push_back(make_shared<Bulk_quote>(Bulk_quote("sss", i*10.1, 10, 0.3)));
 
-    /**
-     * @brief ex15.29   outccome == 6363
-     */
-    std::vector<std::shared_ptr<Quote>> pv;
-
-    for(unsigned i =1; i != 10; ++i)
-        pv.push_back(std::make_shared<Bulk_quote>(Bulk_quote("sss", i * 10.1, 10, 0.3)));
-
-    double total_p = 0;
-    for (auto p : pv)
-    {
-        total_p +=  p->net_price(20);
-    }
-    std::cout << total_p << std::endl;
-
-    return 0;
-
+    double total_p=0;
+    for(const auto& pe : pv)
+        total_p+=pe->net_price(20);
+    cout<<total_p<<endl;
 }
