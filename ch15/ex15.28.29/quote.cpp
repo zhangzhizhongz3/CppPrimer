f3b32751325f7b9c0cdde8e4023d8e0dfd5c8bca
zzz <zhangzhizhongz3@foxmail.com>
2016-09-04 20:04:12 +0800

    Update quote.cpp

--- ch15/ex15.28.29/quote.cpp
+++ ch15/ex15.28.29/quote.cpp
@@ -1,8 +1,16 @@
-#include "quote.h"
+#include <iostream>
+#include <cstddef>
+#include "Bulk_quote.h"
+using namespace std;
 
-void Quote::debug() const
+double Bulk_quote::net_price(size_t n) const
 {
-    std::cout //<< "data members of this class:\n"
-              << "bookNo= " <<this->bookNo << " "
-              << "price= " <<this->price<< " \n";
+    return n*price*(n>=quantity ? 1-discount : 1);
+}
+
+void Bulk_quote::debug() const
+{
+    Quote::debug();
+    cout<<"min_qty= "<<quantity<<" "
+        <<"discount= "<<discount<<" ";
 }
