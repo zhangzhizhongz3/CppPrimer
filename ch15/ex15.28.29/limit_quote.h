5a251dbc95eee9933cf6cc3ae0fc6729444a2814
zzz <zhangzhizhongz3@foxmail.com>
2016-09-04 20:03:27 +0800

    Update limit_quote.h

--- ch15/ex15.28.29/limit_quote.h
+++ ch15/ex15.28.29/limit_quote.h
@@ -1,19 +1,17 @@
-#ifndef LIMIT_QUOTE_H
-#define LIMIT_QUOTE_H
+#ifndef Limit_quote_h
+#define Limit_quote_h
 
-#include "disc_quote.h"
+#include <string>
+#include <cstddef>
+#include "Disc_quote.h"
+using namespace std;
 
-class Limit_quote : public Disc_quote
-{
+class Limit_quote : public Disc_quote {
 public:
-    Limit_quote() = default;
-    Limit_quote(const std::string& b, double p, std::size_t max, double disc):
-        Disc_quote(b,p,max,disc)  {   }
-
-    double net_price(std::size_t n) const override
-    { return n * price * (n < quantity ? 1 - discount : 1 ); }
-
+    Limit_quote()=default;
+    Limit_quote(const string& b, double p, size_t q, double d) : Disc_quote(b, p, q, d) {}
+    double net_price(size_t) const override;
     void debug() const override;
 };
 
-#endif // LIMIT_QUOTE_H
+#endif // Limit_quote_h
