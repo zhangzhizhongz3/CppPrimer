c0b35856fa9150093a654aff90b9eed3310874e0
zzz <zhangzhizhongz3@foxmail.com>
2016-09-04 17:06:59 +0800

    Update main.cpp

--- ch15/ex15.26/main.cpp
+++ ch15/ex15.26/main.cpp
@@ -1,13 +1,6 @@
-/***************************************************************************
- *  @file       main.cpp
- *  @author     Alan.W
- *  @date       27  Jan 2014
- *  @remark     This code is for the exercises from C++ Primer 5th Edition
- *  @note
- ***************************************************************************/
 //!
 //! Exercise 15.26:
-//! Define the Quote and Bulk_quote copy-control members to do the same job
+//! Define the Quote and Bulk_quote copy control members to do the same job
 //! as the synthesized versions. Give them and the other constructors print
 //! statements that identify which function is running. Write programs using
 //! these classes and predict what objects will be created and destroyed.
@@ -15,22 +8,15 @@
 //! until your predictions are reliably correct.
 //!
 
-
-#include <iostream>
-#include <string>
-
-#include "quote.h"
-#include "bulk_quote.h"
-#include "limit_quote.h"
-#include "disc_quote.h"
-
+#include "Quote.h"
+#include "Disc_quote.h"
+#include "Bulk_quote.h"
+#include "Limit_quote.h"
+using namespace std;
 
 int main()
 {
     Bulk_quote bq1;
-    Bulk_quote bq2("ss",2.05,12,0.3);
-    bq2 = std::move(bq2);
-
-
-    return 0;
+    Bulk_quote bq2("ss", 2.05, 12, 0.3);
+    bq2=std::move(bq2);
 }
