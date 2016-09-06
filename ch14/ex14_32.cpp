8cfcff43e0d5287e233a4ad6e61aff3d9040d22b
zzz <zhangzhizhongz3@foxmail.com>
2016-09-06 10:50:32 +0800

    Update ex14_32.cpp

--- ch14/ex14_32.cpp
+++ ch14/ex14_32.cpp
@@ -1,6 +1,7 @@
+#include <iostream>
 #include "ex14_32.h"
 #include "ex14_30_StrBlob.h"
-#include <iostream>
+using namespace std;
 
 StrBlobPtr& StrBlobPtr_pointer::operator*()
 {
@@ -17,5 +18,5 @@ int main()
     StrBlob sb{"hello", "world"};
     StrBlobPtr iter = sb.begin();
     StrBlobPtr_pointer p(&iter);
-    std::cout << p->deref() << std::endl;
+    cout << p->operator*() << endl;
 }
