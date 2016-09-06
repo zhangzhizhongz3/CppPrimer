1f668800cd4bfe8e9c526b6c2ea73201a8581809
zzz <zhangzhizhongz3@foxmail.com>
2016-09-06 15:53:51 +0800

    Update ex14_38_39.cpp

--- ch14/ex14_38_39.cpp
+++ ch14/ex14_38_39.cpp
@@ -1,10 +1,3 @@
-/***************************************************************************
- *  @file       main.cpp
- *  @author     Alan.W
- *  @date       20  Jan 2014
- *  @remark     This code is for the exercises from C++ Primer 5th Edition
- *  @note
- ***************************************************************************/
 //!
 //! Exercise 14.38:
 //! Write a class that tests whether the length of a given string matches a
@@ -17,36 +10,36 @@
 //!
 
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
 #include <fstream>
+#include <string>
+#include <algorithm>
+using namespace std;
 
 class BoundTest {
 public:
-    BoundTest(std::size_t l = 0, std::size_t u = 0) : lower(l), upper(u) {}
-    bool operator()(const std::string& s)
+    BoundTest(size_t l=0, size_t u=0) : lower(l), upper(u) {}
+    bool operator()(const string &s)
     {
-        return lower <= s.length() && s.length() <= upper;
+        return lower<=s.length()&&s.length()<=upper;
     }
-
 private:
-    std::size_t lower;
-    std::size_t upper;
+    size_t lower;
+    size_t upper;
 };
 
 int main()
 {
-    std::ifstream fin("../data/storyDataFile.txt");
-
-    std::size_t quantity9 = 0, quantity10 = 0;
+    ifstream fin("E:\\zzz.txt");
+    size_t quantity9=0, quantity10=0;
     BoundTest test9(1, 9);
     BoundTest test10(1, 10);
 
-    for (std::string word; fin >> word;) {
-        if (test9(word)) ++quantity9;
-        if (test10(word)) ++quantity10;
+    for(string word; fin>>word; )
+    {
+        if(test9(word))
+            ++quantity9;
+        if(test10(word))
+            ++quantity10;
     }
-
-    std::cout << quantity9 << ", " << quantity10 << std::endl;
+    cout<<quantity9<<", "<<quantity10<<endl;
 }
