3d995d8dfd510c7bdfbca35aa32425eaab6f5f05
zzz <zhangzhizhongz3@foxmail.com>
2016-09-06 10:51:18 +0800

    Update ex14_32.h

--- ch14/ex14_32.h
+++ ch14/ex14_32.h
@@ -1,28 +1,21 @@
-/***************************************************************************
- *  @file       StrBlobPtr_pointer.h
- *  @author     Alan.W
- *  @date       19  JAN 2014
- *  @remark     a class that holds a pointer to a StrBlobPtr.
- ***************************************************************************/
+#ifndef StrBlobPtr_pointer_h
+#define StrBlobPtr_pointer_h
 
-#ifndef STRBLOBPTR_POINTER_H
-#define STRBLOBPTR_POINTER_H
+#include "ex14_30_StrBlob.h"
 
 class StrBlobPtr;
 
-/**
- * @brief a class that holds a pointer to a StrBlobPtr.
- */
+//@brief a class that holds a pointer to a StrBlobPtr
 class StrBlobPtr_pointer {
 public:
-    StrBlobPtr_pointer() = default;
+    StrBlobPtr_pointer()=default;
     StrBlobPtr_pointer(StrBlobPtr* p) : pointer(p) {}
 
     StrBlobPtr& operator*();
     StrBlobPtr* operator->();
 
 private:
-    StrBlobPtr* pointer = nullptr;
-};
+    StrBlobPtr* pointer=nullptr;
+} ;
 
-#endif // STRBLOBPTR_POINTER_H
+#endif // StrBlobPtr_pointer_h
