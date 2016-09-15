effe6505e417023124ed3ff41451c64dcf2cdfda
zzz <zhangzhizhongz3@foxmail.com>
2016-09-15 12:25:25 +0800

    Update ex14_43.cpp

--- ch14/ex14_43.cpp
+++ ch14/ex14_43.cpp
@@ -1,10 +1,3 @@
-/***************************************************************************
- *  @file       main.cpp
- *  @author     XDXX， Yue Wang
- *  @date       5/24/2015
- *  @remark     This code is for the exercises from C++ Primer 5th Edition
- *  @note
- ***************************************************************************/
 //!
 //! Exercise 14.43:
 //! Using library function objects, determine whether a given int value is
@@ -12,19 +5,18 @@
 //!
 
 #include <iostream>
-#include <string>
-#include <functional>
+#include <vector>
 #include <algorithm>
+#include <functional>
 
 int main()
 {
-    auto data = {2, 3, 4, 5};
+    std::vector<int> data = {2, 3, 4, 5};
     int input;
     std::cin >> input;
     std::modulus<int> mod;
     auto predicator = [&](int i) { return 0 == mod(input, i); };
-    auto is_divisible = std::any_of(data.begin(), data.end(), predicator);
+    auto is_divisible = std::all_of(data.begin(), data.end(), predicator);
     std::cout << (is_divisible ? "Yes!" : "No!") << std::endl;
-
-    return 0;
 }
+
